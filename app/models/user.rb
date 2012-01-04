f0b9be2c1ccbf9142a602d571e3b38321c47182a
Kazuya NUMATA <numata@gmail.com>
2012-01-04 14:16:37 +0900

    Fix indent(TAB -> two spaces).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,41 @@
 class User < ActiveRecord::Base
-	acts_as_tagger
-	attr_accessor :password_plain, :password_confirmation
-	has_many :items
-	has_many :monthly_profit_losses
-	has_many :accounts
-	has_many :credit_relations
+  acts_as_tagger
+  attr_accessor :password_plain, :password_confirmation
+  has_many :items
+  has_many :monthly_profit_losses
+  has_many :accounts
+  has_many :credit_relations
 
-	N_("User|Password plain")
-	N_("User|Password confirmation")
+  N_("User|Password plain")
+  N_("User|Password confirmation")
 
-	validate :validate_everytime
-	validates_presence_of :login
-	validates_presence_of :password_plain, :if => :password_required?
-	validates_presence_of :email
-	validates_format_of :login, :with => /^[A-Za-z0-9_-]+$/
-	validates_length_of :login, :in =>3..10
-	validates_format_of :password_plain, :with => /^[A-Za-z0-9_-]+$/, :if => :password_required?
-	validates_length_of :password_plain, :in =>6..10, :if => :password_required?
-	validates_uniqueness_of :login, :message => N_("%{fn[:attribute]} has already been used. Input another UserID.")
+  validate :validate_everytime
+  validates_presence_of :login
+  validates_presence_of :password_plain, :if => :password_required?
+  validates_presence_of :email
+  validates_format_of :login, :with => /^[A-Za-z0-9_-]+$/
+  validates_length_of :login, :in =>3..10
+  validates_format_of :password_plain, :with => /^[A-Za-z0-9_-]+$/, :if => :password_required?
+  validates_length_of :password_plain, :in =>6..10, :if => :password_required?
+  validates_uniqueness_of :login, :message => N_("%{fn[:attribute]} has already been used. Input another UserID.")
 
-	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-	validates_length_of :email, :in =>5..255
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_length_of :email, :in =>5..255
 
-	before_save :hash_password
+  before_save :hash_password
 
-	def validate_everytime
-		errors.add("password_plain", _("%{fn[:password_plain]} is not same.")) if self.password_required? && self.password_plain != self.password_confirmation
-	end
+  def validate_everytime
+    errors.add("password_plain", _("%{fn[:password_plain]} is not same.")) if self.password_required? && self.password_plain != self.password_confirmation
+  end
 
-	def password_required?
-		self.password.blank? || !self.password_plain.blank?
-	end
+  def password_required?
+    self.password.blank? || !self.password_plain.blank?
+  end
 
-	def hash_password
-		return if self.password_plain.blank?
-		self.password = CommonUtil.crypt(login + self.password_plain)
-	end
+  def hash_password
+    return if self.password_plain.blank?
+    self.password = CommonUtil.crypt(login + self.password_plain)
+  end
 
   def get_separated_accounts
     accounts = self.accounts.active.order("account_type, order_no")
