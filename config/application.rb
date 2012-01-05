cba328f499ccb19586f98f729e0cc958eb9c5fca
Kazuya NUMATA <numata@gmail.com>
2012-01-06 01:05:13 +0900

    update rails to 3.1.3.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -39,7 +39,7 @@ module Kakeibo3
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    #config.filter_parameters += [:password]
+    config.filter_parameters += [:password]
 
     # Enable the asset pipeline
     config.assets.enabled = true
