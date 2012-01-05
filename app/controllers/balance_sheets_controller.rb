e7d412dd249789e0e6285f363cde3b906a43e2fc
Kazuya NUMATA <numata@gmail.com>
2012-01-06 02:42:03 +0900

    Fix indents

--- app/controllers/balance_sheets_controller.rb
+++ app/controllers/balance_sheets_controller.rb
@@ -3,51 +3,51 @@ class BalanceSheetsController < ApplicationController
   before_filter :required_login
   before_filter :set_separated_accounts, :only => [:show]
   
-	#
-	# balance sheet
-	#
-	def index
-		year = (params[:year] || today.year).to_i
-		month = (params[:month] || today.month).to_i
+  #
+  # balance sheet
+  #
+  def index
+    year = (params[:year] || today.year).to_i
+    month = (params[:month] || today.month).to_i
     @this_month = Date.new(year, month)
 
-		mpls = @user.monthly_profit_losses.where("month <= ?", @this_month)
+    mpls = @user.monthly_profit_losses.where("month <= ?", @this_month)
 
-		@bs = { }
-		@bs.default = 0
-		mpls.each do |mpl|
-			@bs[mpl.account_id] += mpl.amount
-		end
+    @bs = { }
+    @bs.default = 0
+    mpls.each do |mpl|
+      @bs[mpl.account_id] += mpl.amount
+    end
 
-		@accounts = @user.accounts.order("order_no").where(:account_type => 'account')
-		@bs_plus = { }
-		@bs_minus = { }
+    @accounts = @user.accounts.order("order_no").where(:account_type => 'account')
+    @bs_plus = { }
+    @bs_minus = { }
 
-		@plus = []
-		@minus = []
-		@total_plus = 0
-		@total_minus = 0
-		@accounts.each do |a|
-			if @bs[a.id] < 0
-				@minus.push [a, @bs[a.id]]
-				@total_minus += @bs[a.id]
-			else
-				@plus.push [a, @bs[a.id]]
-				@total_plus += @bs[a.id]
-			end
-		end
+    @plus = []
+    @minus = []
+    @total_plus = 0
+    @total_minus = 0
+    @accounts.each do |a|
+      if @bs[a.id] < 0
+        @minus.push [a, @bs[a.id]]
+        @total_minus += @bs[a.id]
+      else
+        @plus.push [a, @bs[a.id]]
+        @total_plus += @bs[a.id]
+      end
+    end
 
-		render :layout => 'entries'
-	rescue ArgumentError => ex # 日付変換等のエラーがあるため
-		redirect_to current_entries_path
-	end
+    render :layout => 'entries'
+  rescue ArgumentError => ex # 日付変換等のエラーがあるため
+    redirect_to current_entries_path
+  end
   
   
   def show
-		if params[:id].blank?
+    if params[:id].blank?
       redirect_rjs_to login_url
-			return
-		else
+      return
+    else
       @account_id = params[:id].to_i
 
       if params[:year].blank? || params[:month].blank?
@@ -60,7 +60,6 @@ class BalanceSheetsController < ApplicationController
       
       @remain_amount, @items = Item.collect_account_history(@user, @account_id, from_date, to_date)
       @from_date = from_date
-		end
+    end
   end
-
 end
