9aac0a86292b1f938cef3bc04ef55b977c8e4694
Kazuya NUMATA <numata@gmail.com>
2012-01-06 21:33:19 +0900

    Refactoring.
    Thare were lots of confusing and redundant codes, so fix them.

--- app/models/account.rb
+++ app/models/account.rb
@@ -13,54 +13,62 @@ class Account < ActiveRecord::Base
   scope :active, where(:is_active => true)
 
   #
-  # 残高を取得する
-  # my_id でitemのIDを指定すると、そのItemが除外される。また、new_dateと、my_idに該当するitemのaction_dateが同一の場合、
-  # new_dateと同じ日のitemデータのうち、my_idよりidがおおきいものは残高計算から除外する
+  # 特定の日付までの残高を取得する
+  # my_id でitemのIDを指定すると、そのItemが除外される。また、
+  # dateと、my_idに該当するitemのaction_dateが同一の場合、
+  # dateと同じ日のitemデータのうち、my_idよりidがおおきいものは残高計算から除外する
   #
-  def self.asset(user, account_id, new_date, my_id=nil) 
-
-    date = new_date
-    # amountの算出
-    # 前月までのassetを算出
-    asset = user.monthly_profit_losses.where(:account_id => account_id).months_before(new_date.beginning_of_month).sum("amount")
-
+  def self.asset(user, account_id, date, my_id=nil) 
     my_item = my_id ? user.items.find_by_id(my_id) : nil
 
+    # amountの算出
+    # 前月までのassetを算出
+    asset = self.asset_to_last_month_except_self(user, account_id, my_item, date)
+
     # 今月のassetの変化を算出
-    # 残高情報の新規登録の場合
-    outgo_items = income_items = user.items
-    
     if my_item.nil?
-      outgo_items = outgo_items.action_date_between(date.beginning_of_month, date).where(:from_account_id => account_id)
-      income_items = income_items.action_date_between(date.beginning_of_month, date).where(:to_account_id => account_id)
+      outgo = user.items.action_date_between(date.beginning_of_month, date).where(:from_account_id => account_id).sum(:amount)
+      income = user.items.action_date_between(date.beginning_of_month, date).where(:to_account_id => account_id).sum(:amount)
+      asset += income - outgo
     else
       if  my_item.action_date == date
-        outgo_items = outgo_items.where("(action_date >= ? and action_date < ?) or (action_date = ? and id < ?)", date.beginning_of_month, date, date, my_id).where(:from_account_id => account_id)
-        income_items = income_items.where("(action_date >= ? and action_date < ?) or (action_date = ? and id < ?)", date.beginning_of_month, date, date, my_id).where(:to_account_id => account_id)
+        asset += self.asset_to_item_of_this_month_except_self(user, account_id, my_id, date)
       else # 日付が変更になった場合
-        outgo_items = outgo_items.action_date_between(date.beginning_of_month, date).where("id <> ?", my_id).where(:from_account_id => account_id)
-        income_items = income_items.action_date_between(date.beginning_of_month, date).where("id <> ?", my_id).where(:to_account_id => account_id)
-      end
-    end
-
-    outgo = outgo_items.sum("amount")
-    income = income_items.sum("amount")
-    outgo ||= 0
-    income ||= 0
-    
-    asset += income - outgo
-
-    #
-    # 今月以前はplから抽出してしまうため、SQLではmy_item.amountを除外できない
-    #
-    if my_item && my_item.action_date < date.beginning_of_month
-      if my_item.from_account_id == account_id
-        asset += my_item.amount
-      else
-        asset -= my_item.amount
+        asset += self.asset_to_date_of_this_month_except_self(user, account_id, my_id, date)
       end
     end
 
     return asset
+  end
+
+  private
+  def self.asset_to_last_month_except_self(user, account_id, item, date)
+    #
+    # 今月以前はplから抽出してしまうため、SQLではmy_item.amountを除外できない
+    #
+    user.monthly_profit_losses.where(account_id: account_id).months_before(date.beginning_of_month).sum(:amount) +
+      self.correlate_for_self(account_id, item, date.beginning_of_month)
+  end
+  
+  def self.correlate_for_self(account_id, item, this_month)
+    retval = 0
+    if item && item.action_date < this_month
+      retval = item.from_account_id == account_id ? item.amount : (-1 * item.amount)
+    end
+    retval
+  end
+
+  def self.asset_to_date_of_this_month_except_self(user, account_id, item_id, date)
+    items_scope = user.items.action_date_between(date.beginning_of_month, date).where("id <> ?", item_id)
+    outgo = items_scope.where(from_account_id: account_id).sum(:amount)
+    income = items_scope.where(to_account_id: account_id).sum(:amount)
+    income - outgo
+  end
+
+  def self.asset_to_item_of_this_month_except_self(user, account_id, item_id, date)
+    items_scope = user.items.where("(action_date >= ? and action_date < ?) or (action_date = ? and id < ?)", date.beginning_of_month, date, date, item_id)
+    outgo = items_scope.where(from_account_id: account_id).sum(:amount)
+    income = items_scope.where(to_account_id: account_id).sum(:amount)
+    income - outgo
   end
 end
