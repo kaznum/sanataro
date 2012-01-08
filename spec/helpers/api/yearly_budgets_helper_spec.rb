02165321d6bdfe72c19dd472294561140d595859
Kazuya NUMATA <numata@gmail.com>
2012-01-09 04:16:14 +0900

    Remove pending declaration because there is no pending spec.

--- spec/helpers/api/yearly_budgets_helper_spec.rb
+++ spec/helpers/api/yearly_budgets_helper_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the Api::YearlyBudgetsHelper. For example:
-#
-# describe Api::YearlyBudgetsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe Api::YearlyBudgetsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
