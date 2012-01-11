a27d948d0150c52b93af51e817678477df2929bb
Kazuya NUMATA <numata@gmail.com>
2012-01-12 03:01:43 +0900

    Change old-style Hash to current 1.9 style hash.

--- app/models/teller.rb
+++ app/models/teller.rb
@@ -14,28 +14,24 @@ class Teller
       MonthlyProfitLoss.correct(user, item.from_account_id, item.action_date.beginning_of_month)
       MonthlyProfitLoss.correct(user, item.to_account_id, item.action_date.beginning_of_month)
       
-      from_item_adj = Item.update_future_balance(user, item.action_date, item.from_account_id, item.id)
-      to_item_adj = Item.update_future_balance(user, item.action_date, item.to_account_id, item.id)
+      from_item_adj = Item.update_future_balance(user, item.action_date,
+                                                 item.from_account_id, item.id)
+      to_item_adj = Item.update_future_balance(user, item.action_date,
+                                               item.to_account_id, item.id)
       affected_items << from_item_adj if from_item_adj
       affected_items << to_item_adj if to_item_adj
-      
 
-      #
       # クレジットカードの処理
-      #
       cr = user.credit_relations.find_by_credit_account_id(item.from_account_id)
       unless cr.nil?
         payment_date = credit_payment_date(user, item.from_account_id, item.action_date)
         cr_item, cr_affected_items, is_cr_error =
-          create_entry(:name => item.name,
-                       :from_account_id => cr.payment_account_id,
-                       :to_account_id => item.from_account_id,
-                       :amount => item.amount,
-                       :action_date => payment_date,
-                       :parent_id => item.id,
-                       :user => user)
+          create_entry(name: item.name, from_account_id: cr.payment_account_id,
+                       to_account_id: item.from_account_id, amount: item.amount,
+                       action_date: payment_date, parent_id: item.id,
+                       user: user)
         raise ActiveRecord::RecordInvalid.new(cr_item) if is_cr_error
-        unless cr_item.nil?
+        if cr_item
           item.child_id = cr_item.id
           item.save!
           affected_items << cr_item
@@ -51,5 +47,4 @@ class Teller
   def self.credit_payment_date(user, account_id, date)
     user.accounts.where(id: account_id).first.credit_due_date(date)
   end
-  
 end
