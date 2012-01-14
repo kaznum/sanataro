d42d9e1152614bbba146cb44bf0b63c8160d7c72
Kazuya NUMATA <numata@gmail.com>
2012-01-14 14:51:15 +0900

    Remove @charaset definition, and fix indent.

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 class Mailer < ActionMailer::Base
-  @@charset='iso-2022-jp'
   def signup_confirmation(user)
     @user = user
     mail(to: user.email, from: SYSTEM_MAIL_ADDRESS, subject: "[#{PRODUCT_NAME}] ユーザ登録は完了していません")
-   end
-   def signup_complete(user)
-     @user = user
-     mail(to: user.email, from: SYSTEM_MAIL_ADDRESS, subject: "[#{PRODUCT_NAME}] ユーザ登録完了")
-   end
-   
+  end
+  
+  def signup_complete(user)
+    @user = user
+    mail(to: user.email, from: SYSTEM_MAIL_ADDRESS, subject: "[#{PRODUCT_NAME}] ユーザ登録完了")
+  end
 end
