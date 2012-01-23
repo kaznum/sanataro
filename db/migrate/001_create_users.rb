d5437e8ea39c333ccced1741738fe1fe1d0f6109
Kazuya NUMATA <numata@gmail.com>
2012-01-24 07:08:36 +0900

    Remove needless and misplaced User.create from db/migrate/001_create_users.rb.
    There is no reason why the User.create is placed in migration files.
    This kinda data should be in seed, and the person who wrote this suckin' codes should be killed(it's me)

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,18 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
-	create_table(:users, {}) {|table|
-		table.column :login,	:string
-		table.column :password,	:string
-		table.column :regist_datetime,	:datetime
-		table.column :is_active,	:boolean, :default=>true
-	}
-
-	User.create(:login=>"numata", :password=>'BYB4xEX6lMpTs') #123456
+    create_table(:users, {}) {|table|
+      table.column :login,	:string
+      table.column :password,	:string
+      table.column :regist_datetime,	:datetime
+      table.column :is_active,	:boolean, :default=>true
+    }
   end
 
   def self.down
-	drop_table :users
+    drop_table :users
   end
-
 end
 
