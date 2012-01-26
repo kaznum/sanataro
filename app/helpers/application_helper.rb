72accd684b83490d175a944541144813831b7292
Kazuya NUMATA <numata@gmail.com>
2012-01-26 10:09:49 +0900

    Move JavaScriptGenerator method to ApplicationHelper because JavaScriptGenerator is not regular class and the instance of JavaScriptGenerator absorb the called messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,24 @@ module ApplicationHelper
   def today
     @view_cached_today ||= Date.today
   end
+
+  
+  def fadeout_and_remove(selector)
+    # 表示されていない可能性があるため、Collection Proxyを利用する
+    self.select(selector).each do |etty|
+      etty.visual_effect :fade, :duration => FADE_DURATION
+    end
+    self.select(selector).each do |etty|
+      self.delay(3.seconds) do
+        etty.remove
+      end
+    end
+  end
+
+  def highlight(selector)
+    debugger
+    self.select(selector).each do |etty|
+      etty.visual_effect :highlight, :duration => HIGHLIGHT_DURATION
+    end
+  end
 end
