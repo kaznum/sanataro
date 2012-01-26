3f8269605d10bf530c30fa1cf45763047de095b8
Kazuya NUMATA <numata@gmail.com>
2012-01-27 00:59:09 +0900

    Replace prototype.js with jQuery.

--- spec/views/account_statuses/destroy.rjs_spec.rb
+++ spec/views/account_statuses/destroy.rjs_spec.rb
@@ -8,8 +8,8 @@ describe "/account_statuses/destroy" do
   end
 
   subject {  rendered }
-  it { should have_prototype_rjs_of :visual_effect, :slide_up, "account_status_body", :duration => '0.2'}
-  it { should have_prototype_rjs_of :replace_html, "account_status" }
+  it { should =~ /\$\("#account_status_body"\)\.slideUp\(200\);/}
+  it { should =~ /\$\("#account_status"\).html\(/ }
   it { should have_prototype_rjs_of :delay, 0.2}
 end
 
--- spec/views/account_statuses/show.rjs_spec.rb
+++ spec/views/account_statuses/show.rjs_spec.rb
@@ -9,7 +9,7 @@ describe "/account_statuses/show" do
   end
 
   subject {  rendered }
-  it { should have_prototype_rjs_of :replace_html, "account_status" }
-  it { should have_prototype_rjs_of :visual_effect, :slide_down, "account_status_body", :duration => '0.2'}
+  it { should =~ /\$\("#account_status"\).html\(/ }
+  it { should =~ /\$\("#account_status_body"\)\.slideDown\(200\);/}
 end
 
