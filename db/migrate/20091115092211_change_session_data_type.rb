c6fe10251423181a10fc5e9af75c58901e68181c
Kazuya NUMATA <numata@gmail.com>
2012-02-08 11:38:52 +0900

    Remove needless session column change from migration.
    These migration files are not only unnecessory, but also depend on some products of DBMS software.
    I wonder who made these suckin' evil deeds.(There is no doubt he was me of those days.)

--- db/migrate/20091115092211_change_session_data_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSessionDataType < ActiveRecord::Migration
-  def self.up
-    execute "ALTER TABLE sessions CHANGE COLUMN data data LONGTEXT"
-  end
-
-  def self.down
-    execute "ALTER TABLE sessions CHANGE COLUMN data data TEXT"
-  end
-end
--- db/migrate/20110930070004_change_session_data_to_text.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSessionDataToText < ActiveRecord::Migration
-  def up
-    change_column :sessions, :data, :text
-  end
-
-  def down
-    execute "ALTER TABLE sessions CHANGE COLUMN data data LONGTEXT"
-  end
-end
