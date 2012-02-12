558dd5b34d8624e3dc03f93989536845303eacb3
Kazuya NUMATA <numata@gmail.com>
2012-02-12 21:35:50 +0900

    Upgrade cucumber and change to use "step" to call other steps.

--- features/step_definitions/entries_steps.rb
+++ features/step_definitions/entries_steps.rb
@@ -3,9 +3,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "pat
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
 
 前提 /^収支入力ページを表示している$/ do
-  Given %{I am on 収支入力}
-  And %{I fill in "login" with "user1"}
-  And %{I fill in "password" with "123456"}
-  And %{I press "ログイン"}
+  step %{I am on 収支入力}
+  step %{I fill in "login" with "user1"}
+  step %{I fill in "password" with "123456"}
+  step %{I press "ログイン"}
 end
 
--- features/step_definitions/web_steps_ja.rb
+++ features/step_definitions/web_steps_ja.rb
@@ -24,132 +24,132 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "pat
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
 
 前提 /^"([^"]*)"ページを表示している$/ do |page_name|
-  Given %{I am on #{page_name}}
+  step %{I am on #{page_name}}
 end
 
 もし /^"([^"]*)"ページを表示する$/ do |page_name|
-  When %{I go to #{page_name}}
+  step %{I go to #{page_name}}
 end
 
 もし /^"([^"]*)"ボタンをクリックする$/ do |button|
-  When %{I press "#{button}"}
+  step %{I press "#{button}"}
 end
 
 もし /^"([^"]*)"リンクをクリックする$/ do |link|
-  When %{I follow "#{link}"}
+  step %{I follow "#{link}"}
 end
 
 もし /^"([^"]*)"の"([^"]*)"リンクをクリックする$/ do |parent, link|
-  When %{I follow "#{link}" within "#{parent}"}
+  step %{I follow "#{link}" within "#{parent}"}
 end
 
 もし /^"([^"]*)"に"([^"]*)"と入力する$/ do |field, value|
-  When %{I fill in "#{field}" with "#{value}"}
+  step %{I fill in "#{field}" with "#{value}"}
 end
 
 # less difference to %{I fill in "#{value}" for "#{field}"} with above in Japanese.
 
 もし /^以下の項目を入力する:$/ do |fields|
-  When %{I fill in the following:}, fields
+  step %{I fill in the following:}, fields
 end
 
 もし /^"([^"]*)"から"([^"]*)"を選択する$/ do |field, value|
-  When %{I select "#{value}" from "#{field}"}
+  step %{I select "#{value}" from "#{field}"}
 end
 
 もし /^日時として"([^"]*)"を選択する$/ do |time|
-  When %{I select "#{time}" as the date and time}
+  step %{I select "#{time}" as the date and time}
 end
 
 もし /^"([^"]*)"の日時として"([^"]*)"を選択する$/ do |datetime_label, datetime|
-  When %{I select "#{datetime}" as the "#{datetime_label}" date and time}
+  step %{I select "#{datetime}" as the "#{datetime_label}" date and time}
 end
 
 もし /^時間として"([^"]*)"を選択する$/ do |time|
-  When %{I select "#{time}" as the time}
+  step %{I select "#{time}" as the time}
 end
 
 もし /^"([^"]*)"の時間として"([^"]*)"を選択する$/ do |time_label, time|
-  When %{I select "#{time}" as the "#{time_label}" time}
+  step %{I select "#{time}" as the "#{time_label}" time}
 end
 
 もし /^日付として"([^"]*)"を選択する$/ do |date|
-  When %{I select "#{date}" as the date}
+  step %{I select "#{date}" as the date}
 end
 
 もし /^"([^"]*)"の日付として"([^"]*)"を選択する$/ do |date_label, date|
-  When %{I select "#{date}" as the "#{date_label}" date}
+  step %{I select "#{date}" as the "#{date_label}" date}
 end
 
 もし /^"([^"]*)"をチェックする$/ do |field|
-  When %{I check "#{field}"}
+  step %{I check "#{field}"}
 end
 
 もし /^"([^"]*)"のチェックを外す$/ do |field|
-  When %{I uncheck "#{field}"}
+  step %{I uncheck "#{field}"}
 end
 
 もし /^"([^"]*)"を選択する$/ do |field|
-  When %{I choose "#{field}"}
+  step %{I choose "#{field}"}
 end
 
 もし /^"([^"]*)"としてファイル"([^"]*)"を選択する$/ do |field, path|
-  When %{I attach the file "#{path}" to "#{field}"}
+  step %{I attach the file "#{path}" to "#{field}"}
 end
 
 ならば /^"([^"]*)"と表示されていること$/ do |text|
-  Then %{I should see "#{text}"}
+  step %{I should see "#{text}"}
 end
 
 ならば /^"([^"]*)"に"([^"]*)"と表示されていること$/ do |selector, text|
-  Then %{I should see "#{text}" within "#{selector}"}
+  step %{I should see "#{text}" within "#{selector}"}
 end
 
 ならば /^\/([^\/]*)\/と表示されていること$/ do |regexp|
-  Then %{I should see /#{regexp}/}
+  step %{I should see /#{regexp}/}
 end
 
 ならば /^"([^"]*)"に\/([^\/]*)\/と表示されていること$/ do |selector, regexp|
-  Then %{I should see \/#{regexp}\/ within "#{selector}"}
+  step %{I should see \/#{regexp}\/ within "#{selector}"}
 end
 
 ならば /^"([^"]*)"と表示されていないこと$/ do |text|
-  Then %{I should not see "#{text}"}
+  step %{I should not see "#{text}"}
 end
 
 ならば /^"([^"]*)"に"([^"]*)"と表示されていないこと$/ do |selector, text|
-  Then %{I should not see "#{text}" within "#{selector}"}
+  step %{I should not see "#{text}" within "#{selector}"}
 end
 
 ならば /^\/([^\/]*)\/と表示されていないこと$/ do |regexp|
-  Then %{I should not see /#{regexp}/}
+  step %{I should not see /#{regexp}/}
 end
 
 ならば /^"([^"]*)"に\/([^\/]*)\/と表示されていないこと$/ do |selector, regexp|
-  Then %{I should not see \/#{regexp}\/ within "#{selector}"}
+  step %{I should not see \/#{regexp}\/ within "#{selector}"}
 end
 
 ならば /^入力項目"([^"]*)"に"([^"]*)"と表示されていること$/ do |field, value|
-  Then %{the "#{field}" field should contain "#{value}"}
+  step %{the "#{field}" field should contain "#{value}"}
 end
 
 ならば /^入力項目"([^"]*)"に"([^"]*)"と表示されていないこと$/ do |field, value|
-  Then %{the "#{field}" field should not contain "#{value}"}
+  step %{the "#{field}" field should not contain "#{value}"}
 end
 
 ならば /^"([^"]*)"がチェックされていること$/ do |label|
-  Then %{the "#{label}" checkbox should be checked}
+  step %{the "#{label}" checkbox should be checked}
 end
 
 ならば /^"([^"]*)"がチェックされていないこと$/ do |label|
-  Then %{the "#{label}" checkbox should not be checked}
+  step %{the "#{label}" checkbox should not be checked}
 end
 
 ならば /^"([^"]*)"ページを表示していること$/ do |page_name|
-  Then %{I should be on #{page_name}}
+  step %{I should be on #{page_name}}
 end
 
-show_me_the_page = lambda { Then %{show me the page} }
+show_me_the_page = lambda { step %{show me the page} }
 
 ならば /^ページを表示する$/, &show_me_the_page
 ならば /^画面を目視$/, &show_me_the_page
