bf7add560f3b1f065d3b7b4cce1e18d36f346a96
Kazuya NUMATA <numata@gmail.com>
2012-02-16 22:35:23 +0900

    i18n

--- app/models/credit_relation.rb
+++ app/models/credit_relation.rb
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 class CreditRelation < ActiveRecord::Base
-  SETTLEMENT_DAYS = PAYMENT_DAYS = [['1', 1], ['2', 2], ['3', 3], ['4', 4], ['5', 5], ['6', 6], ['7', 7], ['8', 8], ['9', 9], ['10', 10], ['11', 11], ['12', 12], ['13', 13], ['14', 14], ['15', 15], ['16', 16], ['17', 17], ['18', 18], ['19', 19], ['20', 20], ['21', 21], ['22', 22], ['23', 23], ['24', 24], ['25', 25], ['26', 26], ['27', 27], ['28', 28], ['末日', 99]]
-  PAYMENT_MONTHS =  [['同月', 0], ['翌月', 1], ['翌々月', 2], ['翌々々月', 3]]
+  SETTLEMENT_DAYS = PAYMENT_DAYS = [['1', 1], ['2', 2], ['3', 3], ['4', 4], ['5', 5],
+                                    ['6', 6], ['7', 7], ['8', 8], ['9', 9], ['10', 10],
+                                    ['11', 11], ['12', 12], ['13', 13], ['14', 14], ['15', 15],
+                                    ['16', 16], ['17', 17], ['18', 18], ['19', 19], ['20', 20],
+                                    ['21', 21], ['22', 22], ['23', 23], ['24', 24], ['25', 25],
+                                    ['26', 26], ['27', 27], ['28', 28], [I18n.t('label.final_day'), 99]]
+  PAYMENT_MONTHS =  [[I18n.t('label.same_month'), 0],
+                     [I18n.t('label.next_month'), 1],
+                     [I18n.t('label.month_after_next'), 2],
+                     [I18n.t('label.two_month_after_next'), 3]]
 
-  validate :validate_everytime
+  validate :validate_on_save
   validates_uniqueness_of   :credit_account_id
   validates_numericality_of :credit_account_id
   validates_numericality_of :payment_account_id
-  validates_inclusion_of :payment_month, :in=>0..3
+  validates_inclusion_of :payment_month, in: 0..3
 
-  def validate_everytime
+  def validate_on_save
     if settlement_day < 1 || (settlement_day > 28 && settlement_day != 99)
-      errors.add(:settlement_day, "が不正な値です。")
+      errors.add(:settlement_day, I18n.t('errors.messages.invalid'))
     end
     if payment_day < 1 || (payment_day > 28 && payment_day != 99)
-      errors.add(:payment_day, "が不正な値です。")
+      errors.add(:payment_day, I18n.t('errors.messages.invalid'))
     end
-    # 引き落としが同月で締め日が引き落とし日より大きい場合はエラー
     if payment_month == 0 && payment_day < settlement_day
-      errors.add("settlement_day", "引き落とし日は締め日後でなければなりません。")
+      errors.add("settlement_day", I18n.t('errors.messages.before_due'))
     end
-    # クレジットカードと引き落としが同じ場合はエラー
     if payment_account_id == credit_account_id
-      errors.add("credit_account_id","クレジットカードと引き落とし口座は別でなければなりません。")
+      errors.add("credit_account_id", I18n.t("errors.messages.different_from"))
     end
   end
 end
--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,11 +2,14 @@
 class Mailer < ActionMailer::Base
   def signup_confirmation(user)
     @user = user
-    mail(to: user.email, from: Settings.system_mail_address, subject: "[#{Settings.product_name}] ユーザ登録は完了していません")
+    mail(to: user.email, from: Settings.system_mail_address, subject: "[#{Settings.product_name}] #{I18n.t('mailer.subject.not_registered_user_yet')}")
   end
   
   def signup_complete(user)
     @user = user
-    mail(to: user.email, from: Settings.system_mail_address, subject: "[#{Settings.product_name}] ユーザ登録完了")
+    mail(to: user.email, from: Settings.system_mail_address, subject: "[#{Settings.product_name}] #{I18n.t('mailer.subject.registered_user_successfully')}")
   end
 end
+
+
+
