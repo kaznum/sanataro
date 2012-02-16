ade7f882786140149090915779b130d20b9a63d1
Kazuya NUMATA <numata@gmail.com>
2012-02-16 19:54:46 +0900

    Extract generating relative month expression to Helper.

---
+++ app/helpers/settings/credit_relations_helper.rb
@@ -0,0 +1,5 @@
+module Settings::CreditRelationsHelper
+  def localize_relative_month(relative_month)
+    CreditRelation::PAYMENT_MONTHS.find{|pm| pm[1] == relative_month }[0]
+  end
+end
---
+++ spec/helpers/settings/credit_relations_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Settings::CreditRelationsHelper do
+  describe "this month" do
+    subject { helper.localize_relative_month(0) }
+    it { should == I18n.t('settings.credit_relations.same_month') }
+  end
+  
+  describe "next month" do
+    subject { helper.localize_relative_month(1) }
+    it { should == I18n.t('settings.credit_relations.next_month')}
+  end
+  
+  describe "the month after next month" do
+    subject { helper.localize_relative_month(2) }
+    it { should == I18n.t('settings.credit_relations.month_after_next')}
+  end
+
+  describe "three month later" do
+    subject { helper.localize_relative_month(3)}
+    it { should == I18n.t('settings.credit_relations.two_month_after_next') }
+  end
+end
