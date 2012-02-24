a7c8b5044bb60e2ee92e319598e6ca887085f215
Kazuya NUMATA <numata@gmail.com>
2012-02-24 10:21:22 +0900

    change initial is_adjustment type to boolean
    PostgreSQL has own boolean type, so it's impossible to put 'f'

--- db/migrate/015_add_adjustment_to_items.rb
+++ db/migrate/015_add_adjustment_to_items.rb
@@ -1,9 +1,11 @@
 class AddAdjustmentToItems < ActiveRecord::Migration
   def self.up
-	add_column :items, :is_adjustment, :integer, :default=>false
-	add_column :items, :adjustment_amount, :integer, :default=>0
+    add_column :items, :is_adjustment, :boolean, :default=>false
+    add_column :items, :adjustment_amount, :integer, :default=>0
   end
 
   def self.down
+    remove_column :items, :is_adjustment
+    remove_column :items, :adjustment_amount
   end
 end
--- db/migrate/016_change_is_adjustmnet_type.rb
+++
@@ -1,8 +0,0 @@
-class ChangeIsAdjustmnetType < ActiveRecord::Migration
-  def self.up
-	change_column :items, :is_adjustment, :boolean, :default=>false
-  end
-
-  def self.down
-  end
-end
