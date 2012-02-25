1dbfe1df9a6b0743202d60930f8ba3b55b614915
Kazuya NUMATA <numata@gmail.com>
2012-02-25 12:34:09 +0900

    Make use gmail for sending email in Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,8 @@ Kakeibo3::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.delivery_method = :sendmail
+# commented out because gmail is used for heroku
+#  config.action_mailer.delivery_method = :sendmail
 
   config.filter_parameters += [:password]
 
