60a33f5d29c9137032e2bae48e2cd934e13cc19f
Kazuya NUMATA <numata@gmail.com>
2012-02-26 02:57:14 +0900

    Basic authentication with ENV.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,20 @@
 class Admin::UsersController < ApplicationController
+  before_filter :authenticate
   def index
     @users = User.all
     render :action => :index, :layout => 'admin'
   end
+
+  private
+  def authenticate
+    if ENV['ADMIN_USER'].blank? || ENV['ADMIN_PASSWORD'].blank?
+      redirect_to login_url
+      return false
+    end
+    
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USER'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end    
+
 end
