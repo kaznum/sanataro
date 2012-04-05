eec3dd3187f65e1074283522df9f7554947779e6
Kazuya NUMATA <numata@gmail.com>
2012-04-05 12:24:52 +0900

    Remove unused lib.

--- lib/acts_as_taggable_redux/acts_as_taggable_redux.rb
+++ lib/acts_as_taggable_redux/acts_as_taggable_redux.rb
@@ -3,7 +3,6 @@ require 'acts_as_tagger'
 
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::Taggable)
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::Tagger)
-ActionView::Base.send(:include, ActsAsTaggableHelper)
 
 require 'tagging'
 require 'tag'
