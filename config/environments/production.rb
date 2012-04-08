cab2f3aa13337fbd0fcc674b9143b0f4aa0bee60
Kazuya NUMATA <numata@gmail.com>
2012-04-08 20:18:37 +0900

    add missing assets precompile items

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Kakeibo3::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( colorPicker.css flot.js piechart.js ejs.js view.js entries_new_simple.css entries_new_simple.js )
+  config.assets.precompile += %w( farbtastic.js farbtastic.css settings/accounts.js flot.js flot/excanvas.min.js piechart.js linechart.js profit_losses.js balance_sheets.js entries_new_simple.css entries_new_simple.js items.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
