f178ba59a6fd9bdf22f5568db754d15070b4c7d3
Kazuya NUMATA <numata@gmail.com>
2012-05-17 18:12:18 +0900

    Change the places where html escape is applied.
    Emolett helper should replace specific strings to HTML tag because another filter could be applied.

--- app/helpers/emolett_helper.rb
+++ app/helpers/emolett_helper.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 module EmolettHelper
   def emolettise(str)
-    (h str).gsub(/\((笑|爆|嬉|喜|楽|驚|泣|涙|悲|怒|厳|辛|苦|閃|汗|忙|急|輝)\)/) { |s| 
+    str.gsub(/\((笑|爆|嬉|喜|楽|驚|泣|涙|悲|怒|厳|辛|苦|閃|汗|忙|急|輝)\)/) { |s| 
       "<span class='emo'>#{s}</span>" 
     }.html_safe
   end
