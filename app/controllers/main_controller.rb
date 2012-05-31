5d5244537bf6998f777547dc6dab2554e7e6ec66
Kazuya NUMATA <numata@gmail.com>
2012-05-31 23:58:16 +0900

    Refactoring.
    Extract methods.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,19 +4,16 @@ class MainController < ApplicationController
   before_filter :_redirect_if_id_is_blank!, only: [:show_parent_child_item]
 
   def show_parent_child_item
-    id = params[:id].to_i
-    type = params[:type].presence || "parent"
-
-    item = type == "parent" ? @user.items.find_by_id(id).try(:parent_item) : @user.items.find_by_parent_id(id)
-
-    if item
-      redirect_js_to entries_url(:year => item.action_date.year, :month => item.action_date.month) + "#item_#{item.id}"
-    else
-      redirect_js_to login_url
-    end
+    redirect_js_to parent_child_of_item_url(params[:id].to_i, params[:type]) || login_url
   end
 
   private
+
+  def parent_child_of_item_url(id, type = "parent")
+    item = type == "parent" ? @user.items.find_by_id(id).try(:parent_item) : @user.items.find_by_parent_id(id)
+    item ? entries_url(year: item.action_date.year, month: item.action_date.month) + "#item_#{item.id}" : nil
+  end
+
   def _redirect_if_id_is_blank!
     if params[:id].blank?
       redirect_js_to login_url
