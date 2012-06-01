b233c8e5cf6b515f7829f3707d35132ac07f9f42
Kazuya NUMATA <numata@gmail.com>
2012-06-01 21:44:09 +0900

    Load libraries correctly just after dependent libraries are loaded with ActiveSupport.on_load

--- lib/sanataro_taggable/init.rb
+++ lib/sanataro_taggable/init.rb
@@ -1,10 +1,13 @@
 $:.unshift File.dirname(__FILE__)
 
-require 'sanataro_taggable'
-require 'sanataro_tagger'
+ActiveSupport.on_load(:active_record) do
+  require 'sanataro_taggable'
+  require 'sanataro_tagger'
 
-ActiveRecord::Base.send(:include, ActiveRecord::Sanataro::Taggable)
-ActiveRecord::Base.send(:include, ActiveRecord::Sanataro::Tagger)
+  ActiveRecord::Base.send(:include, ActiveRecord::Sanataro::Taggable)
+  ActiveRecord::Base.send(:include, ActiveRecord::Sanataro::Tagger)
 
-require 'tagging'
-require 'tag'
+  require 'tagging'
+  require 'tag'
+end
+
