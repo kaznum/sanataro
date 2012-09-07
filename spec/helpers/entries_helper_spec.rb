e1f9f20a63e02d32481e0af2505dcfd03ea03018
Kazuya NUMATA <numata@gmail.com>
2012-09-07 09:31:43 +0900

    fix level of spec of a method

--- spec/helpers/entries_helper_spec.rb
+++ spec/helpers/entries_helper_spec.rb
@@ -2,58 +2,60 @@
 require 'spec_helper'
 
 describe EntriesHelper do
-  fixtures :users, :accounts
-  before do
-    @item = Fabricate.build(:item, amount: 1500, from_account_id: 1, to_account_id: 3,)
-    @item.save!
-    @item.reload
-  end
-
-  context "when neither tag nor mark nor keyword is specified," do
-    context "when now confirmation_required is true," do
-      subject { helper.link_to_confirmation_required(@item.id, true) }
-      it { should ==  link_to(I18n.t('label.confirmation_mark'), entry_confirmation_required_path(@item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+  describe "#link_to_confirmation_required" do
+    fixtures :users, :accounts
+    before do
+      @item = Fabricate.build(:item, amount: 1500, from_account_id: 1, to_account_id: 3,)
+      @item.save!
+      @item.reload
     end
 
-    context "when now confirmation_required is false," do
-      subject { helper.link_to_confirmation_required(@item.id, false) }
-      it { should ==  link_to(I18n.t('label.no_confirmation_mark'), entry_confirmation_required_path(@item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
-    end
-  end
+    context "when neither tag nor mark nor keyword is specified," do
+      context "when now confirmation_required is true," do
+        subject { helper.link_to_confirmation_required(@item.id, true) }
+        it { should ==  link_to(I18n.t('label.confirmation_mark'), entry_confirmation_required_path(@item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+      end
 
-  context "when tag is specified," do
-    context "when now confirmation_required is true," do
-      subject { helper.link_to_confirmation_required(@item.id, true, tag: "tagtag" ) }
-      it { should ==  link_to(I18n.t('label.confirmation_mark'), tag_entry_confirmation_required_path("tagtag", @item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
-    end
-
-    context "when now confirmation_required is false," do
-      subject { helper.link_to_confirmation_required(@item.id, false, tag: "tagtag" ) }
-      it { should ==  link_to(I18n.t('label.no_confirmation_mark'), tag_entry_confirmation_required_path("tagtag", @item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
-    end
-  end
-
-  context "when mark is specified," do
-    context "when now confirmation_required is true," do
-      subject { helper.link_to_confirmation_required(@item.id, true, mark: "markmark" ) }
-      it { should ==  link_to(I18n.t('label.confirmation_mark'), mark_entry_confirmation_required_path("markmark", @item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+      context "when now confirmation_required is false," do
+        subject { helper.link_to_confirmation_required(@item.id, false) }
+        it { should ==  link_to(I18n.t('label.no_confirmation_mark'), entry_confirmation_required_path(@item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
+      end
     end
 
-    context "when now confirmation_required is false," do
-      subject { helper.link_to_confirmation_required(@item.id, false, mark: "markmark" ) }
-      it { should ==  link_to(I18n.t('label.no_confirmation_mark'), mark_entry_confirmation_required_path("markmark", @item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
-    end
-  end
-  
-  context "when keyword is specified," do
-    context "when now confirmation_required is true," do
-      subject { helper.link_to_confirmation_required(@item.id, true, keyword: "keykey" ) }
-      it { should ==  link_to(I18n.t('label.confirmation_mark'), keyword_entry_confirmation_required_path("keykey", @item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+    context "when tag is specified," do
+      context "when now confirmation_required is true," do
+        subject { helper.link_to_confirmation_required(@item.id, true, tag: "tagtag" ) }
+        it { should ==  link_to(I18n.t('label.confirmation_mark'), tag_entry_confirmation_required_path("tagtag", @item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+      end
+
+      context "when now confirmation_required is false," do
+        subject { helper.link_to_confirmation_required(@item.id, false, tag: "tagtag" ) }
+        it { should ==  link_to(I18n.t('label.no_confirmation_mark'), tag_entry_confirmation_required_path("tagtag", @item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
+      end
     end
 
-    context "when now confirmation_required is false," do
-      subject { helper.link_to_confirmation_required(@item.id, false, keyword: "keykey" ) }
-      it { should ==  link_to(I18n.t('label.no_confirmation_mark'), keyword_entry_confirmation_required_path("keykey", @item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
+    context "when mark is specified," do
+      context "when now confirmation_required is true," do
+        subject { helper.link_to_confirmation_required(@item.id, true, mark: "markmark" ) }
+        it { should ==  link_to(I18n.t('label.confirmation_mark'), mark_entry_confirmation_required_path("markmark", @item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+      end
+
+      context "when now confirmation_required is false," do
+        subject { helper.link_to_confirmation_required(@item.id, false, mark: "markmark" ) }
+        it { should ==  link_to(I18n.t('label.no_confirmation_mark'), mark_entry_confirmation_required_path("markmark", @item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
+      end
+    end
+    
+    context "when keyword is specified," do
+      context "when now confirmation_required is true," do
+        subject { helper.link_to_confirmation_required(@item.id, true, keyword: "keykey" ) }
+        it { should ==  link_to(I18n.t('label.confirmation_mark'), keyword_entry_confirmation_required_path("keykey", @item.id, confirmation_required: false), remote: true, method: :put, class: 'item_confirmation_required') }
+      end
+
+      context "when now confirmation_required is false," do
+        subject { helper.link_to_confirmation_required(@item.id, false, keyword: "keykey" ) }
+        it { should ==  link_to(I18n.t('label.no_confirmation_mark'), keyword_entry_confirmation_required_path("keykey", @item.id, confirmation_required: true), remote: true, method: :put, class: 'item_confirmation_not_required') }
+      end
     end
   end
 end
