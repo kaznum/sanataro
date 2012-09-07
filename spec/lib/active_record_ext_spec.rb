2e46d6b412d88bee9da3f59a8e9eda8e41323582
Kazuya NUMATA <numata@gmail.com>
2012-09-07 10:54:03 +0900

    add missing spec for Arel Extension

---
+++ spec/lib/active_record_ext_spec.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe ActiveRecordExt do
+  describe "LIKE clause" do
+    subject { User.arel_table[:name].matches("%my!%name%").to_sql }
+    it { should match /\s+ESCAPE '!'$/ }
+  end
+
+  describe "NOT LIKE clause" do
+    subject { User.arel_table[:name].does_not_match("%my!%name%").to_sql }
+    it { should match /\s+ESCAPE '!'$/ }
+  end
+end
+
