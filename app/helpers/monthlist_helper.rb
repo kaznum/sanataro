e6b9ba0be037bf8336b61da11811baf3436f9fa4
Kazuya NUMATA <numata@gmail.com>
2012-09-10 18:09:54 +0900

    move #monthlist div tag into helper because it is meaningless without the inner elements

--- app/helpers/monthlist_helper.rb
+++ app/helpers/monthlist_helper.rb
@@ -1,6 +1,6 @@
 module MonthlistHelper
   def monthlist(from_year, from_month, to_year, to_month, selected_year, selected_month, current_action='items')
-    out = "<div id='years'>" + from_year.upto(to_year).map {|y| link_to(y, "#year_#{y}", {class: (y == selected_year ? "selected" : "unselected") }) }.join(" | ") + "</div>"
+    out = "<div id='monthlist'><div id='years'>" + from_year.upto(to_year).map {|y| link_to(y, "#year_#{y}", {class: (y == selected_year ? "selected" : "unselected") }) }.join(" | ") + "</div>"
     year = from_year
     month = from_month
     out += "<div id='year_#{year}' style='display: #{year == selected_year ? 'block;' : 'none;'}'>"
@@ -9,7 +9,7 @@ module MonthlistHelper
         out += month.upto(to_month).map { |m|
           link_to_unless(selected_year == y && selected_month == m, m.to_s, :action => current_action, :year => y, :month=>m)
         }.join(" | ")
-        out += "</div>"
+        out += "</div></div>"
       else
         out += month.upto(12).map { |m|
           link_to_unless(selected_year == y && selected_month == m, m.to_s, :action => current_action, :year => y, :month=>m)
@@ -21,3 +21,4 @@ module MonthlistHelper
     out.html_safe
   end
 end
+
