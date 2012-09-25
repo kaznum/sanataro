6879b56e47931e98e16526d7d14a5ec9529b5aed
Kazuya NUMATA <numata@gmail.com>
2012-09-25 16:45:39 +0900

    SQL Server OFFSET Support
    offset should be used with limit

--- app/models/item.rb
+++ app/models/item.rb
@@ -31,7 +31,10 @@ class Item < ActiveRecord::Base
   scope :action_date_between, lambda { |from, to| where(action_date: from..to) }
   scope :confirmation_required, where(confirmation_required: true)
   scope :default_limit, limit(Settings.item_list_count)
-  scope :remaining, offset(Settings.item_list_count)
+  # FIX ME
+  #
+  # limit is fixed number.
+  scope :remaining, offset(Settings.item_list_count).limit(999999)
   scope :order_of_entries, order(arel_table[:action_date].desc).order(arel_table[:id].desc)
 
   def validates_action_date_range
