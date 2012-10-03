c213908b6a4a9a3da2bc866313010160ec604204
Kazuya NUMATA <numata@gmail.com>
2012-10-03 17:15:59 +0900

    Fix the locations to call html_safe

--- lib/string_decorator.rb
+++ lib/string_decorator.rb
@@ -4,8 +4,8 @@ module StringDecorator
     def emolettise(str)
       str = str.html_safe? ? str : (ERB::Util.html_escape str)
       str.gsub(/(\(|（)(笑|爆|嬉|喜|楽|驚|泣|涙|悲|怒|厳|辛|苦|閃|汗|忙|急|輝)(\)|）)/) { |s|
-        "<span class='emo'>#{s}</span>".html_safe
-      }
+        "<span class='emo'>#{s}</span>"
+      }.html_safe
     end
   end
 end
@@ -17,6 +17,6 @@ class String
   end
 
   def emolettise
-    StringDecorator.emolettise(self).html_safe
+    StringDecorator.emolettise(self)
   end
 end
--- spec/lib/string_decorator_spec.rb
+++ spec/lib/string_decorator_spec.rb
@@ -6,11 +6,13 @@ describe StringDecorator do
     context "when string is already escaped, " do
       subject { "aaa<a href='aa'>bbb</a>(笑)ccc".html_safe.emolettise }
       it { should == "aaa<a href='aa'>bbb</a><span class='emo'>(笑)</span>ccc" }
+      it { should be_html_safe }
     end
 
     context "when string is not escaped yet, " do
       subject { "aaa<a href='aa'>bbb</a>(笑)ccc".emolettise }
       it { should == "aaa&lt;a href=&#x27;aa&#x27;&gt;bbb&lt;/a&gt;<span class='emo'>(笑)</span>ccc" }
+      it { should be_html_safe }
     end
   end
 
@@ -18,11 +20,13 @@ describe StringDecorator do
     context "when string is already escaped, " do
       subject { "aaa<a href='aa'>bbb</a>(笑)ccc:sushi:ddd".html_safe.decorate }
       it { should match /aaa<a href='aa'>bbb<\/a><span class='emo'>\(笑\)<\/span>ccc<img src=[^>]+>ddd/ }
+      it { should be_html_safe }
     end
 
     context "when string is not escaped yet, " do
       subject { "aaa<a href='aa'>bbb</a>(笑)ccc:sushi:ddd".decorate }
       it { should match /aaa&lt;a href=&#x27;aa&#x27;&gt;bbb&lt;\/a&gt;\<span class='emo'\>\(笑\)\<\/span\>ccc\<img src=[^>]+\>ddd/ }
+      it { should be_html_safe }
     end
   end
 end
