ab8954aff37d69bb1db70ba344db91f225abc150
Kazuya NUMATA <numata@gmail.com>
2012-10-04 11:00:12 +0900

    Remove emoji controller and its views because they are out of date

--- app/controllers/emojis_controller.rb
+++
@@ -1,9 +0,0 @@
-class EmojisController < ApplicationController
-  before_filter :required_login
-
-  def index
-    if params[:form_id].blank?
-      redirect_js_to(current_entries_url) and return
-    end
-  end
-end
