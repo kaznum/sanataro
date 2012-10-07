3b2e1b4002bb6fadae5480fd80c04f82fdadbd9d
Kazuya NUMATA <numata@gmail.com>
2012-10-07 23:18:34 +0900

    Make Item abstract by implementing update_with_filter! and filter_and_assign_attributes

--- app/models/adjustment_observer.rb
+++ app/models/adjustment_observer.rb
@@ -1,10 +1,4 @@
 class AdjustmentObserver < ActiveRecord::Observer
-  def before_create(item)
-    user = item.user
-    prev_adj = user.adjustments.find_by_to_account_id_and_action_date(item.to_account_id, item.action_date)
-    prev_adj.destroy if prev_adj
 
-    item.fill_amount
-  end
 end
 
