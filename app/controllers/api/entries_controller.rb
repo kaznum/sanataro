7a87aa6f289527421e82dd79983b5afdd7dc824f
Kazuya NUMATA <numata@gmail.com>
2012-10-11 18:31:11 +0900

    Implement handling exceptions

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -5,6 +5,49 @@ class Api::EntriesController < ApplicationController
   def index
     super
     respond_with @items.to_json(:include => [:parent_item, :child_item])
+  rescue ArgumentError
+    render nothing: true, status: :not_acceptable
+  end
+
+  def show
+    super
+    respond_with @item
+  end
+
+  def create
+    _json_action do
+      super
+      render json: @item, status: :created, :location => api_entries_url(@item.id)
+    end
+  end
+
+  def update
+    _json_action do
+      super
+      render json: @item, status: :ok
+    end
+  end
+
+  def destroy
+    _json_action do
+      super
+      render json: @item, status: :ok
+    end
+  end
+
+  private
+  def _json_action(&block)
+    block.call
+  rescue SyntaxError
+    errors = [t("error.amount_is_invalid")]
+    render json: { errors: errors }.to_json, status: :not_acceptable
+  rescue InvalidDate # in case the date in params has invalid format
+    render nothing: true, status: :not_acceptable
+  rescue ActiveRecord::RecordNotFound
+    render nothing: true, status: :not_found
+  rescue ActiveRecord::RecordInvalid => ex
+    errors = ex.error_messages
+    render json: { errors: errors }.to_json, status: :not_acceptable
   end
 end
 
