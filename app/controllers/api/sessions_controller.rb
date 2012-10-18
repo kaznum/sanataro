04e08d0fbfb77ad5d16cc396f292ec2a139e4743
Kazuya NUMATA <numata@gmail.com>
2012-10-18 14:15:52 +0900

    Implement api/sessions#create

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,25 @@
 class Api::SessionsController < ApplicationController
   def create
+    unless params[:session]
+      render nothing: true, status: :unauthorized
+      return
+    end
+
+    login = params[:session][:login]
+    password = params[:session][:password]
+
+    user = User.find_by_login_and_active(login, true)
+
+    if user && user.password_correct?(password)
+      authenticated = true
+    end
+
+    if authenticated
+      session[:user_id] = user.id
+      render nothing: true, status: :ok
+    else
+      render nothing: true, status: :unauthorized
+    end
   end
 
   def destroy
--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -3,4 +3,86 @@ require 'spec_helper'
 
 describe Api::SessionsController do
   fixtures :users
+
+  describe "#create" do
+    context "when params[:session] does not mismatch," do
+      before do
+        @action = -> { post :create, login: users(:user1).login, password: "123456" }
+      end
+
+      describe "response" do
+        before { @action.call }
+        subject { response }
+
+        its(:response_code) { should == 401 }
+      end
+
+      describe "session" do
+        before { @action.call }
+        subject { session }
+
+        its([:user_id]) { should be_nil }
+      end
+    end
+
+    context "when user doesn't exist," do
+      before do
+        @action = -> { post :create, session: { login: "not_exist", password: "not_exist_pass"} }
+      end
+
+      describe "response" do
+        before { @action.call }
+        subject { response }
+
+        its(:response_code) { should == 401 }
+      end
+
+      describe "session" do
+        before { @action.call }
+        subject { session }
+
+        its([:user_id]) { should be_nil }
+      end
+    end
+
+    context "when user exists but password mismatches," do
+      before do
+        @action = -> { post :create, session: { login: users(:user1).login, password: "not_match_pass"} }
+      end
+
+      describe "response" do
+        before { @action.call }
+        subject { response }
+
+        its(:response_code) { should == 401 }
+      end
+
+      describe "session" do
+        before { @action.call }
+        subject { session }
+
+        its([:user_id]) { should be_nil }
+      end
+    end
+
+    context "when user exists and password matches," do
+      before do
+        @action = -> { post :create, session: { login: users(:user1).login, password: "123456"} }
+      end
+
+      describe "response" do
+        before { @action.call }
+        subject { response }
+
+        its(:response_code) { should == 200 }
+      end
+
+      describe "session" do
+        before { @action.call }
+        subject { session }
+
+        its([:user_id]) { should == users(:user1).id }
+      end
+    end
+  end
 end
