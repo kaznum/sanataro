7663b30eda725e1b03404914d90a73f879b1e005
Kazuya NUMATA <numata@gmail.com>
2012-10-19 19:40:46 +0900

    Add spec for OAuth authentication

--- spec/controllers/api/accounts_controller_spec.rb
+++ spec/controllers/api/accounts_controller_spec.rb
@@ -44,6 +44,40 @@ describe Api::AccountsController do
       it_should_behave_like "Unauthenticated Access in API"
     end
 
+    context "with OAuth," do
+      context "when resource_owner_id is correct," do
+        before do
+          token = double
+          token.should_receive(:resource_owner_id).and_return(users(:user1).id)
+          token.should_receive(:accessible?).and_return(true)
+          @controller.define_singleton_method(:doorkeeper_token) do
+            token
+          end
+          get :index, format: :json
+        end
+
+        describe "response" do
+          subject { response }
+          it { should be_success }
+          it { should render_template :index }
+        end
+      end
+
+      context "when resource_owner_id is wrong," do
+        before do
+          token = double
+          token.should_receive(:resource_owner_id).and_return(999999)
+          token.should_receive(:accessible?).and_return(true)
+          @controller.define_singleton_method(:doorkeeper_token) do
+            token
+          end
+          get :index, format: :json
+        end
+
+        it_should_behave_like "Unauthenticated Access in API"
+      end
+    end
+
     context "after login," do
       before do
         login
