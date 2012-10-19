c0555fd35adaf21b3e990859e57f907ea6464f17
Kazuya NUMATA <numata@gmail.com>
2012-10-19 18:06:28 +0900

    Implement authentication of /oauth/applications

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -21,6 +21,33 @@ Doorkeeper.configure do
   #   # routes.new_admin_session_path
   #   Admin.find_by_id(session[:admin_id]) || redirect_to(routes.new_admin_session_url)
   # end
+  admin_authenticator do |routes|
+    def authenticate
+      admin_user, admin_password = get_correct_credential
+      if admin_user.nil? || admin_password.nil?
+        return nil
+      end
+
+      authenticate_or_request_with_http_basic do |username, password|
+        username == admin_user && password == admin_password
+      end
+    end
+
+    def get_correct_credential
+      admin_user =  ENV['OAUTH_ADMIN_USER'].presence
+      admin_password = ENV['OAUTH_ADMIN_PASSWORD'].presence
+
+      begin
+        admin_user ||= Settings.oauth_admin_user
+        admin_password ||= Settings.oauth_admin_password
+      rescue Settingslogic::MissingSetting
+        admin_user = admin_password = nil
+      end
+
+      [admin_user, admin_password]
+    end
+    authenticate || render(text: "Unauthorized", status: :unauthorized)
+  end
 
   # Access token expiration time (default 2 hours).
   # If you want to disable expiration, set this to nil.
@@ -39,4 +66,5 @@ Doorkeeper.configure do
   # fallsback to `:client_id` and `:client_secret` from `params` object
   # Check out the wiki for mor information on customization
   # client_credentials :from_basic, :from_params
+
 end
