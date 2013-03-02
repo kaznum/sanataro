8763765a8ca37684db8ffbdefa97ae0f11088a7c
Kazuya NUMATA <numata@gmail.com>
2013-03-02 19:34:58 +0900

    Exec rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sanataro::Application.config.secret_token = '4bb064a33999a01f264ff4442b3941cfee876b7d67a8ebd062b89915cd223b16e65e873fbb2594523f0a5af3278f7d860558b1829c519e185f26424ff6acb12f'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sanataro::Application.config.secret_key_base = '0037d0aae72d93c8a8574861fe18ea3e7d9bf70220a4832e6528b8c0d7d5a66e5630b12ab7993913fc8b149dfe2ebd9b0e102b064d5efcd76a2f90af9e1c8fbb'
