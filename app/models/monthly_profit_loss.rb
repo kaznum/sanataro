5f2f859dbf9af190f98523043b0faf0fc67adb48
Kazuya NUMATA <numata@gmail.com>
2013-03-02 18:38:41 +0900

    Quit using dynamic method of find_or_initialize_by_...

--- app/models/monthly_profit_loss.rb
+++ app/models/monthly_profit_loss.rb
@@ -5,7 +5,7 @@ class MonthlyProfitLoss < ActiveRecord::Base
 
   class << self
     def correct(user, account_id, month)
-      pl = user.monthly_profit_losses.find_or_initialize_by_account_id_and_month(account_id, month)
+      pl = user.monthly_profit_losses.find_or_initialize_by(account_id: account_id, month: month)
       items_of_month = user.items.where(action_date: month..month.end_of_month)
       pl.amount = items_of_month.where(to_account_id: account_id).sum(:amount) -
         items_of_month.where(from_account_id: account_id).sum(:amount)
