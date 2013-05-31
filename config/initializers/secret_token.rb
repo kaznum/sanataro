fde421becb7f8662cb16af4f7b125aa4aa518ae4
Kazuya NUMATA <numata@gmail.com>
2013-05-31 17:25:19 +0900

    Support SettingsLogic for SECRET_TOKEN settings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Sanataro::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+Sanataro::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || Settings.secret_token
