85a1b7d5daf193723144b1c2931e84594e32958e
Kazuya NUMATA <numata@gmail.com>
2013-06-02 21:57:58 +0900

    Fix the checking logic whether ENV is set of not

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Sanataro::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || Settings.secret_token
+Sanataro::Application.config.secret_key_base = ENV['SECRET_TOKEN'].presence || Settings.secret_token
