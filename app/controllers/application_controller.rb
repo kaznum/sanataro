5eb4588312e2f77c76ca2671b0bbf21723b1cd5a
Kazuya NUMATA <numata@gmail.com>
2013-09-19 06:49:12 +0900

    Change old style hash format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options(options = {})
-    { :locale => I18n.locale }
+    { locale: I18n.locale }
   end
 
   private
