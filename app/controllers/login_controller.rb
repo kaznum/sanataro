03cf5a379b7d5801ee5f978514d6d2bc13332abc
Kazuya NUMATA <numata@gmail.com>
2013-09-18 23:38:19 +0900

    Fix misleading '(..)' expression

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -96,7 +96,7 @@ class LoginController < ApplicationController
     user = _get_user_by_login_and_autologin_key(al_params[:login], al_params[:autologin_key])
     if user
       _do_login(user.login, nil, "1", true, al_params[:only_add])
-      redirect_to (al_params[:only_add] ? simple_input_url : current_entries_url)
+      redirect_to(al_params[:only_add] ? simple_input_url : current_entries_url)
       false
     else
       true
