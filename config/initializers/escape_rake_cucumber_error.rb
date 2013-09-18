4f15231b41f28c4f8ceaa3a93c6462326544a8ea
Kazuya NUMATA <numata@gmail.com>
2013-09-19 00:34:01 +0900

    Remove the error handling because it is not ideal

--- config/initializers/escape_rake_cucumber_error.rb
+++ config/initializers/escape_rake_cucumber_error.rb
@@ -2,24 +2,21 @@
 # Why: http://groups.google.com/group/cukes/browse_thread/thread/5682d41436e235d7
 
 if Rails.env.test?
-  begin
-    require 'minitest/unit'
-    # Don't attempt to monkeypatch if the require succeeded but didn't
-    # define the actual module.
-    #
-    # https://github.com/cucumber/cucumber/pull/93
-    # http://youtrack.jetbrains.net/issue/TW-17414
-    if defined?(MiniTest::Unit)
-      class MiniTest::Unit
-        class << self
-          @@installed_at_exit = true
-        end
+  require 'minitest/unit'
+  # Don't attempt to monkeypatch if the require succeeded but didn't
+  # define the actual module.
+  #
+  # https://github.com/cucumber/cucumber/pull/93
+  # http://youtrack.jetbrains.net/issue/TW-17414
+  if defined?(MiniTest::Unit)
+    class MiniTest::Unit
+      class << self
+        @@installed_at_exit = true
+      end
 
-        def run(*)
-          0
-        end
+      def run(*)
+        0
       end
     end
-  rescue LoadError => ignore
   end
 end
