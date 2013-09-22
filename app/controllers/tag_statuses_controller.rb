ead4b028b96e3e5fd5d2e1fac5d7da3c2769ebc1
Kazuya NUMATA <numata@gmail.com>
2013-09-22 20:47:38 +0900

    Add missing white spaces

--- app/controllers/tag_statuses_controller.rb
+++ app/controllers/tag_statuses_controller.rb
@@ -1,6 +1,6 @@
 class TagStatusesController < ApplicationController
   before_action :required_login
   def show
-    @tags = @user.tags.order(:name).uniq.sort {|a,b| a.name <=> b.name }
+    @tags = @user.tags.order(:name).uniq.sort { |a, b| a.name <=> b.name }
   end
 end
