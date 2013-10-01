1157a1f8097b5735db6bf460059509fc471d27dd
Kazuya NUMATA <numata@gmail.com>
2013-10-01 20:06:26 +0900

    Remove extra spaces

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Admin::UsersController do
-  
   def mock_user
     mock_model(User).as_null_object
   end
@@ -24,7 +23,7 @@ describe Admin::UsersController do
         it {should_not render_template "index"}
       end
     end
-    
+
     context "with authentication data in server's Settings," do
       context "when user/password is correct," do
         before do
@@ -42,7 +41,7 @@ describe Admin::UsersController do
           it {should be_success }
         end
       end
-      
+
       context "when user/password is incorrect," do
         before do
           Settings.should_receive(:admin_user).and_return("admin_setting")
@@ -59,16 +58,14 @@ describe Admin::UsersController do
           its(:status) {should == 401 }
         end
       end
-      
     end
-    
+
     context "with authentication setting in ENV," do
-      
       context "when user/password is incorrect," do
         before do
           ENV['ADMIN_USER'] = 'admin_env'
           ENV['ADMIN_PASSWORD'] = 'password_env'
-          
+
           request.env['HTTP_AUTHORIZATION'] =
             'Basic ' + Base64::encode64("admin:password_env")
         end
@@ -79,16 +76,16 @@ describe Admin::UsersController do
           its(:status) { should == 401 }
         end
       end
-      
+
       context "when user/password is correct," do
         before do
           ENV['ADMIN_USER'] = 'admin'
           ENV['ADMIN_PASSWORD'] = 'password_env'
-          
+
           request.env['HTTP_AUTHORIZATION'] =
             'Basic ' + Base64::encode64("admin:password_env")
         end
-        
+
         describe "response" do
           before do
             get :index
@@ -100,16 +97,15 @@ describe Admin::UsersController do
         end
       end
     end
-    
+
     context "with authentication setting in both ENV and Settings," do
-      
       context "when EVN's user/password pair is specified," do
         before do
           ENV['ADMIN_USER'] = 'admin_env'
           ENV['ADMIN_PASSWORD'] = 'password_env'
           Settings.stub(:admin_user).and_return("admin_setting")
           Settings.stub(:admin_password).and_return("password_setting")
-          
+
           request.env['HTTP_AUTHORIZATION'] =
             'Basic ' + Base64::encode64("admin_env:password_env")
         end
@@ -122,20 +118,19 @@ describe Admin::UsersController do
           it {should be_success}
           it {should render_template "index"}
         end
-
       end
-      
+
       context "when Ssettings user/password pair is specified," do
         before do
           ENV['ADMIN_USER'] = 'admin_env'
           ENV['ADMIN_PASSWORD'] = 'password_env'
           Settings.stub(:admin_user).and_return("admin_setting")
           Settings.stub(:admin_password).and_return("password_setting")
-          
-          request.env['HTTP_AUTHORIZATION'] = 
+
+          request.env['HTTP_AUTHORIZATION'] =
             'Basic ' + Base64::encode64("admin_setting:password_setting")
         end
-        
+
         describe "response" do
           before { get :index }
           subject {response}
@@ -143,7 +138,7 @@ describe Admin::UsersController do
         end
       end
     end
-    
+
     context "when authentication pass," do
       context "when user/password is correct," do
         before do
@@ -151,7 +146,7 @@ describe Admin::UsersController do
             ins.should_receive(:authenticate).and_return(true)
           end
         end
-        
+
         describe "Methods calls" do
           specify do
             User.should_receive(:all).and_return(user_objects)
@@ -169,7 +164,7 @@ describe Admin::UsersController do
           subject { assigns(:users) }
           it { should == user_objects }
         end
-        
+
         describe "response" do
           before do
             get :index
