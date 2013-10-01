a6a8f4acdc70fd3fba7e465ff45cf822ea3b4db7
Kazuya NUMATA <numata@gmail.com>
2013-10-01 20:56:41 +0900

    Remove extra blank lines and white spaces

--- spec/controllers/chart_data/assets_controller_spec.rb
+++ spec/controllers/chart_data/assets_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe ChartData::AssetsController do
   fixtures :users
-  
+
   describe "#show" do
     context "before login," do
       before do
@@ -22,7 +22,7 @@ describe ChartData::AssetsController do
         before do
           get :show, :id => '21222', :format => :json
         end
-        
+
         it_should_behave_like "Not Acceptable"
       end
 
@@ -38,7 +38,7 @@ describe ChartData::AssetsController do
         before do
           get :show, :id => '2008a2', :format => :json
         end
-        
+
         it_should_behave_like "Not Acceptable"
       end
 
@@ -46,7 +46,7 @@ describe ChartData::AssetsController do
         before do
           get :show, :id => '200815', :format => :json
         end
-        
+
         it_should_behave_like "Not Acceptable"
       end
 
@@ -68,18 +68,16 @@ describe ChartData::AssetsController do
           _ = users(:user1).incomes.create!(:name => "その2", :active => true, :order_no => 20)
           account3 = users(:user1).bankings.create!(:name => "その3", :active => true, :order_no => 30)
           account4 = users(:user1).bankings.create!(:name => "その4", :active => true, :order_no => 40)
-          
-          
+
           users(:user1).monthly_profit_losses.create!(:month => Date.new(1999,5), :account_id => account1.id, :amount => -300 )
           users(:user1).monthly_profit_losses.create!(:month => Date.new(1988,6), :account_id => account1.id, :amount => -100 )
           users(:user1).monthly_profit_losses.create!(:month => Date.new(1999,1), :account_id => account1.id, :amount => 900 )
           users(:user1).monthly_profit_losses.create!(:month => Date.new(1999,1), :account_id => account3.id, :amount => 900 )
           users(:user1).monthly_profit_losses.create!(:month => Date.new(1998,3), :account_id => account4.id, :amount => -200 )
-
-          
         end
+
         context "when asset_type is not specify," do
-          before do 
+          before do
             get :show, :id => '199901', :format => :json
           end
 
@@ -92,7 +90,7 @@ describe ChartData::AssetsController do
           end
         end
         context "when asset_type is debt," do
-          before do 
+          before do
             get :show, :id => '199901', :format => :json, :asset_type => 'debt'
           end
 
