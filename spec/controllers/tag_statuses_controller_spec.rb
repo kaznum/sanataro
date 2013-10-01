78376e1a576f6ef5ed0a79129e4f3a9d5f2c4918
Kazuya NUMATA <numata@gmail.com>
2013-10-01 14:17:37 +0900

    Remove missing spaces

--- spec/controllers/tag_statuses_controller_spec.rb
+++ spec/controllers/tag_statuses_controller_spec.rb
@@ -17,26 +17,26 @@ describe TagStatusesController do
         login
         # test data
         create_entry entry: { action_date: '2008/2/3', name: 'テスト1' , amount: '10,000', from_account_id: accounts(:bank1).id, to_account_id: accounts(:expense3).id, tag_list: 'abc def' }, year: 2008, month: 2
-        create_entry entry: { action_date: '2008/2/3',  name: 'テスト2' , amount: '10,000', from_account_id: accounts(:bank1).id, to_account_id: accounts(:expense3).id, tag_list: 'abc '}, year: 2008, month: 2
-        create_entry entry: { action_date: '2008/2/3',  name: 'テスト3' , amount: '10,000', from_account_id: accounts(:bank1).id, to_account_id: accounts(:expense3).id, tag_list: 'def'}, year: 2008, month: 2
+        create_entry entry: { action_date: '2008/2/3',  name: 'テスト2' , amount: '10,000', from_account_id: accounts(:bank1).id, to_account_id: accounts(:expense3).id, tag_list: 'abc ' }, year: 2008, month: 2
+        create_entry entry: { action_date: '2008/2/3',  name: 'テスト3' , amount: '10,000', from_account_id: accounts(:bank1).id, to_account_id: accounts(:expense3).id, tag_list: 'def' }, year: 2008, month: 2
         xhr :get, :show
       end
 
       describe "response" do
         subject { response }
-        it {should be_success }
-        it {should render_template "show"}
+        it { should be_success }
+        it { should render_template "show" }
       end
 
       describe "@tags" do
         subject { assigns(:tags) }
-        it {should_not be_nil}
-        it {should have_at_least(1).tags}
+        it { should_not be_nil }
+        it { should have_at_least(1).tags }
       end
 
       describe "uniqueness" do
         subject { assigns(:tags) }
-        its(:size) { should == assigns(:tags).map(&:name).uniq.size}
+        its(:size) { should == assigns(:tags).map(&:name).uniq.size }
       end
     end
   end
