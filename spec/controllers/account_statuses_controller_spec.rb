c4837253f6708794fb3fe8dc7f467a5aa901bbdc
Kazuya NUMATA <numata@gmail.com>
2013-10-01 14:28:19 +0900

    Change old style hash expressions

--- spec/controllers/account_statuses_controller_spec.rb
+++ spec/controllers/account_statuses_controller_spec.rb
@@ -27,7 +27,7 @@ describe AccountStatusesController do
 
       describe "@account_statuses" do
         before do
-          users(:user1).general_items.create!(:from_account_id => -1, :to_account_id => accounts(:bank1).id, :amount => -100, :action_date => Date.today, :name => 'unknown')
+          users(:user1).general_items.create!(from_account_id: -1, to_account_id: accounts(:bank1).id, amount: -100, action_date: Date.today, name: 'unknown')
           xhr :get, :show
         end
 
