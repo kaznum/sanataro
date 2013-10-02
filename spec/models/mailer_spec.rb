a5f24af8774dc74fd709f7e5bee5c929a7145773
Kazuya NUMATA <numata@gmail.com>
2013-10-03 07:59:56 +0900

    Remove extra white spaces

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
- 
+
 describe Mailer do
   describe "signup_confirmation" do
     let(:user) { mock_model(User, email: "foo@example.com", login: "userhogehoge", confirmation: "AABBCCDD") }
@@ -9,8 +9,8 @@ describe Mailer do
     describe "mail" do
       subject{mail}
       its(:subject) { should == "[#{Settings.product_name}] ユーザ登録は完了していません" }
-      its(:to) {should == [ user.email ]}
-      its(:from) { should == [ Settings.system_mail_address ] }
+      its(:to) {should == [user.email]}
+      its(:from) { should == [Settings.system_mail_address] }
     end
 
     describe "user" do
@@ -22,12 +22,12 @@ describe Mailer do
   describe "signup_complete" do
     let(:user) { mock_model(User, email: "foo@example.com", login: "userhogehoge", confirmation: "AABBCCDD") }
     let(:mail) { Mailer.signup_complete(user) }
-    
+
     describe "mail" do
       subject{mail}
       its(:subject) { should == "[#{Settings.product_name}] ユーザ登録完了" }
-      its(:to) {should == [ user.email ]}
-      its(:from) { should == [ Settings.system_mail_address ] }
+      its(:to) {should == [user.email]}
+      its(:from) { should == [Settings.system_mail_address] }
     end
 
     describe "user" do
