6976288c911c162eed6d676f4dc5ffc8859422f4
Kazuya NUMATA <numata@gmail.com>
2013-10-03 08:20:37 +0900

    Use %r for regular expressions matching more than 1 '/' character

--- spec/lib/string_decorator_spec.rb
+++ spec/lib/string_decorator_spec.rb
@@ -19,13 +19,13 @@ describe StringDecorator do
   describe "String#decorate" do
     context "when string is already escaped, " do
       subject { "aaa<a href='aa'>bbb</a>(笑)ccc:sushi:ddd".html_safe.decorate }
-      it { should match /aaa<a href='aa'>bbb<\/a><span class='emo'>\(笑\)<\/span>ccc<img src=[^>]+>ddd/ }
+      it { should match %r(aaa<a href='aa'>bbb</a><span class='emo'>\(笑\)</span>ccc<img src=[^>]+>ddd) }
       it { should be_html_safe }
     end
 
     context "when string is not escaped yet, " do
       subject { "aaa<a href='aa'>bbb</a>(笑)ccc:sushi:ddd".decorate }
-      it { should match /aaa&lt;a href=&#39;aa&#39;&gt;bbb&lt;\/a&gt;\<span class='emo'\>\(笑\)\<\/span\>ccc\<img src=[^>]+\>ddd/ }
+      it { should match %r(aaa&lt;a href=&#39;aa&#39;&gt;bbb&lt;/a&gt;\<span class='emo'\>\(笑\)\</span\>ccc\<img src=[^>]+\>ddd) }
       it { should be_html_safe }
     end
   end
