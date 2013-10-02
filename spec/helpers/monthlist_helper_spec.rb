c0a0463c53672d7889097c8dfba96bc5db477d78
Kazuya NUMATA <numata@gmail.com>
2013-10-02 22:40:02 +0900

    Change old hash style expressions

--- spec/helpers/monthlist_helper_spec.rb
+++ spec/helpers/monthlist_helper_spec.rb
@@ -11,8 +11,8 @@ describe MonthlistHelper do
     current_action = 'foo'
 
     helper.stub(:link_to).and_return('month_link')
-    helper.should_receive(:link_to).with(2008, "#year_2008", :class => "unselected").and_return("year_2008_link")
-    helper.should_receive(:link_to).with(2009, "#year_2009", :class => "selected").and_return("year_2009_link")
+    helper.should_receive(:link_to).with(2008, "#year_2008", class: "unselected").and_return("year_2008_link")
+    helper.should_receive(:link_to).with(2009, "#year_2009", class: "selected").and_return("year_2009_link")
 
     @returned = helper.monthlist(from_year, from_month, to_year, to_month, selected_year, selected_month, current_action)
   end
@@ -21,3 +21,5 @@ describe MonthlistHelper do
   it { should be =~ /<div class='years'>year_2008_linkyear_2009_link<\/div>/ }
   it { should be =~ /<div class='year_2008' style='display: none;'>(month_link){3}<\/div><div class='year_2009' style='display: block;'>(month_link){12}<\/div>/ }
 end
+
+
