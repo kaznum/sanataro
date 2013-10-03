ac633f64790e1af53bbacb25fe6ef344e864a193
Kazuya NUMATA <numata@gmail.com>
2013-10-03 21:36:39 +0900

    Add missing white spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@ user = User.new( password_plain: "demo123", password_confirmation: "demo123",
 user.login = "demo"
 user.save!
 
-hashed_accounts = [{name: "財布", order_no: 10, type: 'bankings'},
-                   {name: "銀行A", order_no: 20, type: 'bankings'},
-                   {name: "銀行B", order_no: 30, type: 'bankings'},
-                   {name: "クレジットカード", order_no: 30, type: 'bankings'},
-                   {name: "給与", order_no: 10, type: 'incomes'},
-                   {name: "賞与", order_no: 20, type: 'incomes'},
-                   {name: "雑収入", order_no: 30, type: 'incomes'},
-                   {name: "食費", order_no: 10, type: 'expenses'},
-                   {name: "光熱費", order_no: 10, type: 'expenses'},
-                   {name: "住居費", order_no: 10, type: 'expenses'},
-                   {name: "美容費", order_no: 10, type: 'expenses'},
-                   {name: "衛生費", order_no: 10, type: 'expenses'},
-                   {name: "雑費", order_no: 10, type: 'expenses'},
+hashed_accounts = [{ name: "財布", order_no: 10, type: 'bankings' },
+                   { name: "銀行A", order_no: 20, type: 'bankings' },
+                   { name: "銀行B", order_no: 30, type: 'bankings' },
+                   { name: "クレジットカード", order_no: 30, type: 'bankings' },
+                   { name: "給与", order_no: 10, type: 'incomes' },
+                   { name: "賞与", order_no: 20, type: 'incomes' },
+                   { name: "雑収入", order_no: 30, type: 'incomes' },
+                   { name: "食費", order_no: 10, type: 'expenses' },
+                   { name: "光熱費", order_no: 10, type: 'expenses' },
+                   { name: "住居費", order_no: 10, type: 'expenses' },
+                   { name: "美容費", order_no: 10, type: 'expenses' },
+                   { name: "衛生費", order_no: 10, type: 'expenses' },
+                   { name: "雑費", order_no: 10, type: 'expenses' },
                   ]
 
 accounts = []
@@ -25,22 +25,22 @@ hashed_accounts.each do |data|
   accounts << user.send(data[:type].to_sym).create!(name: data[:name], order_no: data[:order_no])
 end
 
-user.credit_relations.create!(credit_account_id: accounts.find{|a| a.name == "クレジットカード" }.id,
-                              payment_account_id: accounts.find{|a| a.name == "銀行B" }.id,
+user.credit_relations.create!(credit_account_id: accounts.find { |a| a.name == "クレジットカード" }.id,
+                              payment_account_id: accounts.find { |a| a.name == "銀行B" }.id,
                               settlement_day: 25, payment_month: 2, payment_day: 4)
 
 user.items.create!(name: '会社給与',
-                   from_account_id: accounts.find{ |a| a.name == "給与"}.id,
-                   to_account_id: accounts.find{|a| a.name == "銀行A" }.id,
+                   from_account_id: accounts.find { |a| a.name == "給与" }.id,
+                   to_account_id: accounts.find { |a| a.name == "銀行A" }.id,
                    amount: 1000, action_date: Date.today)
 
 user.items.create!(name: '妻のへそくりを発見',
-                   from_account_id: accounts.find{ |a| a.name == "雑収入"}.id,
-                   to_account_id: accounts.find{|a| a.name == "財布" }.id,
+                   from_account_id: accounts.find { |a| a.name == "雑収入" }.id,
+                   to_account_id: accounts.find { |a| a.name == "財布" }.id,
                    amount: 800, action_date: Date.yesterday, confirmation_required: true)
 
 user.items.create!(name: 'サンプル支出',
-                   from_account_id: accounts.find{|a| a.name == "財布" }.id,
-                   to_account_id: accounts.find{|a| a.name == "食費" }.id,
+                   from_account_id: accounts.find { |a| a.name == "財布" }.id,
+                   to_account_id: accounts.find { |a| a.name == "食費" }.id,
                    amount: 250, action_date: Date.today, tag_list: 'コンビニ')
 
