de186e623a0c5c339ddb8f0152ceaa32265d37f9
Kazuya NUMATA <numata@gmail.com>
2013-10-03 22:22:30 +0900

    Replace $: with $LOAD_PATH

--- config/initializers/load_libs.rb
+++ config/initializers/load_libs.rb
@@ -1,4 +1,4 @@
-$:.unshift File.dirname(__FILE__)
+$LOAD_PATH.unshift File.dirname(__FILE__)
 
 require File.expand_path(File.join(Rails.root, 'lib', 'common_util'))
 require File.expand_path(File.join(Rails.root, 'lib', 'active_record_ext'))
