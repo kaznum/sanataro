f31acbee58dd51e40563924d1b311b6c3cc2c0f8
Kazuya NUMATA <numata@gmail.com>
2014-06-12 18:01:31 +0900

    Convert specs to RSpec 2.99.0 syntax with Transpec
    This conversion is done by Transpec 2.2.4 with the following command:
        transpec spec/routing
    
    * 64 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 1 conversion
        from: describe 'some routing' { }
          to: describe 'some routing', :type => :routing { }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,79 +1,79 @@
 require 'spec_helper'
 
-describe :routes do
+describe :routes, :type => :routing do
 
   describe "root" do
     describe 'GET /' do
       subject { get('/') }
-      it { should route_to("login#login") }
+      it { is_expected.to route_to("login#login") }
     end
   end
 
   describe 'login' do
     describe 'GET login' do
       subject { get('/login') }
-      it { should route_to(controller: 'login', action: 'login') }
+      it { is_expected.to route_to(controller: 'login', action: 'login') }
     end
 
     describe 'named route' do
       subject { get(login_path) }
-      it { should route_to(controller: 'login', action: 'login') }
+      it { is_expected.to route_to(controller: 'login', action: 'login') }
     end
 
     describe 'POST login' do
       subject { post('/login') }
-      it { should route_to(controller: 'login', action: 'do_login') }
+      it { is_expected.to route_to(controller: 'login', action: 'do_login') }
     end
 
     describe 'named route' do
       subject { post(login_path) }
-      it { should route_to(controller: 'login', action: 'do_login') }
+      it { is_expected.to route_to(controller: 'login', action: 'do_login') }
     end
   end
 
   describe "logout" do
     describe 'POST logout' do
       subject { post('/logout') }
-      it { should route_to(controller: 'login', action: 'do_logout') }
+      it { is_expected.to route_to(controller: 'login', action: 'do_logout') }
     end
 
     describe 'named route' do
       subject { post(logout_path) }
-      it { should route_to(controller: 'login', action: 'do_logout') }
+      it { is_expected.to route_to(controller: 'login', action: 'do_logout') }
     end
   end
 
   describe "create_user" do
     describe 'GET create_user' do
       subject { get('/create_user') }
-      it { should route_to("login#create_user") }
+      it { is_expected.to route_to("login#create_user") }
     end
 
     describe 'named route' do
       subject { get(create_user_path) }
-      it { should route_to("login#create_user") }
+      it { is_expected.to route_to("login#create_user") }
     end
 
     describe 'POST create_user' do
       subject { post('/create_user') }
-      it { should route_to("login#do_create_user") }
+      it { is_expected.to route_to("login#do_create_user") }
     end
 
     describe 'named route' do
       subject { post(create_user_path) }
-      it { should route_to("login#do_create_user") }
+      it { is_expected.to route_to("login#do_create_user") }
     end
   end
 
   describe "login#confirmation" do
     describe 'GET confirm_user' do
       subject { get('/confirm_user') }
-      it { should route_to("login#confirmation") }
+      it { is_expected.to route_to("login#confirmation") }
     end
 
     describe 'named route' do
       subject { get(confirm_user_path) }
-      it { should route_to("login#confirmation") }
+      it { is_expected.to route_to("login#confirmation") }
     end
   end
 
@@ -81,46 +81,46 @@ describe :routes do
     %w(entries profit_losses balance_sheets).each do |controller|
       describe "get /current/#{controller}" do
         subject { get("/current/#{controller}") }
-        it { should route_to("#{controller}#index") }
+        it { is_expected.to route_to("#{controller}#index") }
       end
       describe "post /current/#{controller}" do
         subject { post("/current/#{controller}") }
-        it { should route_to("#{controller}#create") }
+        it { is_expected.to route_to("#{controller}#create") }
       end
       describe "get /current/#{controller}/10" do
         subject { get("/current/#{controller}/10") }
-        it { should route_to("#{controller}#show", id: "10") }
+        it { is_expected.to route_to("#{controller}#show", id: "10") }
       end
       describe "put /current/#{controller}/10" do
         subject { put("/current/#{controller}/10") }
-        it { should route_to("#{controller}#update", id: "10") }
+        it { is_expected.to route_to("#{controller}#update", id: "10") }
       end
 
       describe "delete /current/#{controller}/10" do
         subject { delete("/current/#{controller}/10") }
-        it { should route_to("#{controller}#destroy", id: "10") }
+        it { is_expected.to route_to("#{controller}#destroy", id: "10") }
       end
 
       describe "get /current/#{controller}/10/edit" do
         subject { get("/current/#{controller}/10/edit") }
-        it { should route_to("#{controller}#edit", id: "10") }
+        it { is_expected.to route_to("#{controller}#edit", id: "10") }
       end
     end
 
     describe 'named_route' do
       describe "entries" do
         subject { { get: current_entries_path }}
-        it { should route_to(controller: 'entries', action: 'index') }
+        it { is_expected.to route_to(controller: 'entries', action: 'index') }
       end
 
       describe 'profit_losses' do
         subject { { get: current_profit_losses_path }}
-        it { should route_to(controller: 'profit_losses', action: 'index') }
+        it { is_expected.to route_to(controller: 'profit_losses', action: 'index') }
       end
 
       describe 'balance_sheets' do
         subject { { get: current_balance_sheets_path }}
-        it { should route_to(controller: 'balance_sheets', action: 'index') }
+        it { is_expected.to route_to(controller: 'balance_sheets', action: 'index') }
       end
     end
   end
@@ -128,116 +128,116 @@ describe :routes do
   describe 'months/2000/3/..' do
     describe 'GET "entries"' do
       subject { { get: '/months/2008/3/entries' }}
-      it { should route_to(controller: 'entries', action: 'index', year: "2008", month: "3") }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', year: "2008", month: "3") }
     end
     describe 'GET "profit_losses"' do
       subject { { get: '/months/2008/3/profit_losses' } }
-      it { should route_to(controller: 'profit_losses', action: 'index', year: "2008", month: "3") }
+      it { is_expected.to route_to(controller: 'profit_losses', action: 'index', year: "2008", month: "3") }
     end
 
     describe 'GET "balance_sheets"' do
       subject { { get: '/months/2008/3/balance_sheets' } }
-      it { should route_to(controller: 'balance_sheets', action: 'index', year: "2008", month: "3") }
+      it { is_expected.to route_to(controller: 'balance_sheets', action: 'index', year: "2008", month: "3") }
     end
   end
 
   describe '/tags/:tag/..' do
     describe 'GET "entries"' do
       subject { { get: '/tags/hogehoge/entries' }}
-      it { should route_to(controller: 'entries', action: 'index', tag: 'hogehoge') }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', tag: 'hogehoge') }
     end
 
     describe 'named route' do
       subject { { get: tag_entries_path('hogehoge') }}
-      it { should route_to(controller: 'entries', action: 'index', tag: 'hogehoge') }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', tag: 'hogehoge') }
     end
   end
 
   describe '/tags/:tag/entries/:entry_id/confirmation_required' do
     describe 'PUT "confirmation_required"' do
       subject { { put: '/tags/hogehoge/entries/10/confirmation_required' }}
-      it { should route_to(controller: 'confirmation_requireds', action: 'update', tag: 'hogehoge', entry_id: '10') }
+      it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', tag: 'hogehoge', entry_id: '10') }
     end
 
     describe 'named route' do
       subject { { put: tag_entry_confirmation_required_path('hogehoge', 10) }}
-      it { should route_to(controller: 'confirmation_requireds', action: 'update', tag: 'hogehoge', entry_id: '10') }
+      it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', tag: 'hogehoge', entry_id: '10') }
     end
   end
 
   describe "/marks/:mark/.." do
     describe 'GET "entries"' do
       subject { { get: '/marks/hogehoge/entries' }}
-      it { should route_to(controller: 'entries', action: 'index', mark: 'hogehoge') }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', mark: 'hogehoge') }
     end
 
     describe 'named route' do
       subject { { get: mark_entries_path('hogehoge') }}
-      it { should route_to(controller: 'entries', action: 'index', mark: 'hogehoge') }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', mark: 'hogehoge') }
     end
   end
 
   describe '/marks/:mark/entries/:entry_id/confirmation_required' do
     describe 'PUT "confirmation_required"' do
       subject { { put: '/marks/hogehoge/entries/10/confirmation_required' }}
-      it { should route_to(controller: 'confirmation_requireds', action: 'update', mark: 'hogehoge', entry_id: '10') }
+      it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', mark: 'hogehoge', entry_id: '10') }
     end
 
     describe 'named route' do
       subject { { put: mark_entry_confirmation_required_path('hogehoge', 10) }}
-      it { should route_to(controller: 'confirmation_requireds', action: 'update', mark: 'hogehoge', entry_id: '10') }
+      it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', mark: 'hogehoge', entry_id: '10') }
     end
   end
 
   describe "/keywords/:keyword/.." do
     describe 'GET "entries"' do
       subject { { get: '/keywords/hogehoge/entries' }}
-      it { should route_to(controller: 'entries', action: 'index', keyword: 'hogehoge') }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', keyword: 'hogehoge') }
     end
 
     describe 'named route' do
       subject { { get: keyword_entries_path('hogehoge') }}
-      it { should route_to(controller: 'entries', action: 'index', keyword: 'hogehoge') }
+      it { is_expected.to route_to(controller: 'entries', action: 'index', keyword: 'hogehoge') }
     end
   end
 
   describe '/keywords/:keyword/entries/:entry_id/confirmation_required' do
     describe 'PUT "confirmation_required"' do
       subject { { put: '/keywords/hogehoge/entries/10/confirmation_required' }}
-      it { should route_to(controller: 'confirmation_requireds', action: 'update', keyword: 'hogehoge', entry_id: '10') }
+      it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', keyword: 'hogehoge', entry_id: '10') }
     end
 
     describe 'named route' do
       subject { { put: keyword_entry_confirmation_required_path('hogehoge', 10) }}
-      it { should route_to(controller: 'confirmation_requireds', action: 'update', keyword: 'hogehoge', entry_id: '10') }
+      it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', keyword: 'hogehoge', entry_id: '10') }
     end
   end
 
   describe '/entries/10/confirmation_required' do
     subject { put("/entries/10/confirmation_required") }
-    it { should route_to(controller: 'confirmation_requireds', action: 'update', entry_id: '10') }
+    it { is_expected.to route_to(controller: 'confirmation_requireds', action: 'update', entry_id: '10') }
   end
 
   %w(account_status confirmation_status tag_status).each do |controller|
     describe "get #{controller}" do
       subject { get("/#{controller}") }
-      it { should route_to("#{controller.pluralize}#show") }
+      it { is_expected.to route_to("#{controller.pluralize}#show") }
     end
     describe "destroy #{controller}" do
       subject { delete("/#{controller}") }
-      it { should route_to("#{controller.pluralize}#destroy") }
+      it { is_expected.to route_to("#{controller.pluralize}#destroy") }
     end
   end
 
   describe 'entry_candidates' do
     subject { get("/entry_candidates") }
-    it { should route_to("entry_candidates#index") }
+    it { is_expected.to route_to("entry_candidates#index") }
   end
 
   describe '/admin' do
     describe 'users' do
       subject { get("/admin/users") }
-      it { should route_to("admin/users#index") }
+      it { is_expected.to route_to("admin/users#index") }
     end
   end
 
@@ -245,38 +245,38 @@ describe :routes do
     %w(accounts credit_relations).each do |controller|
       describe "get #{controller}" do
         subject { get("/settings/#{controller}") }
-        it { should route_to("settings/#{controller}#index") }
+        it { is_expected.to route_to("settings/#{controller}#index") }
       end
       describe "post #{controller}" do
         subject { post("/settings/#{controller}") }
-        it { should route_to("settings/#{controller}#create") }
+        it { is_expected.to route_to("settings/#{controller}#create") }
       end
       describe "get #{controller}/10" do
         subject { get("/settings/#{controller}/10") }
-        it { should route_to("settings/#{controller}#show", id: "10") }
+        it { is_expected.to route_to("settings/#{controller}#show", id: "10") }
       end
       describe "put #{controller}/10" do
         subject { put("/settings/#{controller}/10") }
-        it { should route_to("settings/#{controller}#update", id: "10") }
+        it { is_expected.to route_to("settings/#{controller}#update", id: "10") }
       end
       describe "delete #{controller}/10" do
         subject { delete("/settings/#{controller}/10") }
-        it { should route_to("settings/#{controller}#destroy", id: "10") }
+        it { is_expected.to route_to("settings/#{controller}#destroy", id: "10") }
       end
       describe "get #{controller}/10/edit" do
         subject { get("/settings/#{controller}/10/edit") }
-        it { should route_to("settings/#{controller}#edit", id: "10") }
+        it { is_expected.to route_to("settings/#{controller}#edit", id: "10") }
       end
     end
 
     describe "user" do
       describe "get" do
         subject { get("/settings/user") }
-        it { should route_to("settings/users#show") }
+        it { is_expected.to route_to("settings/users#show") }
       end
       describe "put" do
         subject { put("/settings/user") }
-        it { should route_to("settings/users#update") }
+        it { is_expected.to route_to("settings/users#update") }
       end
     end
   end
@@ -285,39 +285,39 @@ describe :routes do
     describe "sessions" do
       describe "create api/session" do
         subject { post("/api/session") }
-        it { should route_to("api/sessions#create") }
+        it { is_expected.to route_to("api/sessions#create") }
       end
 
       describe "destroy api/session" do
         subject { delete("/api/session") }
-        it { should route_to("api/sessions#destroy") }
+        it { is_expected.to route_to("api/sessions#destroy") }
       end
     end
 
     %w(entries accounts).each do |controller|
       describe "get api/#{controller}" do
         subject { get("/api/#{controller}") }
-        it { should route_to("api/#{controller}#index") }
+        it { is_expected.to route_to("api/#{controller}#index") }
       end
       describe "get api/#{controller}/10" do
         subject { get("/api/#{controller}/10") }
-        it { should route_to("api/#{controller}#show", id: "10") }
+        it { is_expected.to route_to("api/#{controller}#show", id: "10") }
       end
       describe "get api/#{controller}/10/edit" do
         subject { get("/api/#{controller}/10/edit") }
-        it { should route_to("api/#{controller}#edit", id: "10") }
+        it { is_expected.to route_to("api/#{controller}#edit", id: "10") }
       end
       describe "put api/#{controller}/10" do
         subject { put("/api/#{controller}/10") }
-        it { should route_to("api/#{controller}#update", id: "10") }
+        it { is_expected.to route_to("api/#{controller}#update", id: "10") }
       end
       describe "delete api/#{controller}/10" do
         subject { delete("/api/#{controller}/10") }
-        it { should route_to("api/#{controller}#destroy", id: "10") }
+        it { is_expected.to route_to("api/#{controller}#destroy", id: "10") }
       end
       describe "post api/#{controller}" do
         subject { post("/api/#{controller}") }
-        it { should route_to("api/#{controller}#create") }
+        it { is_expected.to route_to("api/#{controller}#create") }
       end
     end
   end
@@ -327,27 +327,27 @@ describe :routes do
     %w(assets budgets yearly_assets yearly_budgets).each do |controller|
       describe "get chart_data/#{controller}" do
         subject { get("/chart_data/#{controller}") }
-        it { should route_to("chart_data/#{controller}#index") }
+        it { is_expected.to route_to("chart_data/#{controller}#index") }
       end
       describe "get chart_data/#{controller}/10" do
         subject { get("/chart_data/#{controller}/10") }
-        it { should route_to("chart_data/#{controller}#show", id: "10") }
+        it { is_expected.to route_to("chart_data/#{controller}#show", id: "10") }
       end
       describe "get chart_data/#{controller}/10/edit" do
         subject { get("/chart_data/#{controller}/10/edit") }
-        it { should route_to("chart_data/#{controller}#edit", id: "10") }
+        it { is_expected.to route_to("chart_data/#{controller}#edit", id: "10") }
       end
       describe "put chart_data/#{controller}/10" do
         subject { put("/chart_data/#{controller}/10") }
-        it { should route_to("chart_data/#{controller}#update", id: "10") }
+        it { is_expected.to route_to("chart_data/#{controller}#update", id: "10") }
       end
       describe "delete chart_data/#{controller}/10" do
         subject { delete("/chart_data/#{controller}/10") }
-        it { should route_to("chart_data/#{controller}#destroy", id: "10") }
+        it { is_expected.to route_to("chart_data/#{controller}#destroy", id: "10") }
       end
       describe "post chart_data/#{controller}" do
         subject { post("/chart_data/#{controller}") }
-        it { should route_to("chart_data/#{controller}#create") }
+        it { is_expected.to route_to("chart_data/#{controller}#create") }
       end
     end
   end
