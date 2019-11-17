b6172652e3ff03bd7a80ccd8c124839066a56a07
Kazuya NUMATA <numata@gmail.com>
2019-11-17 17:28:44 +0900

    Refactor twitter button helper by eliminate URI.escape and generate URL by URI class

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,25 +1,37 @@
 # frozen_string_literal: true
 
 module TwitterHelper
-  TWITTER_SHARE_URL = 'http://twitter.com/intent/tweet'
+  TWITTER_SHARE_URL = 'https://twitter.com/intent/tweet'
+
   def tweet_button(item)
-    escaped_url = URI.escape('http://bit.ly/Imx6PT')
+    url = 'http://bit.ly/Imx6PT'
     accounts = item.user.all_accounts
     from = item.from_account_id
     to = item.to_account_id
 
-    escaped_text = if item.user.income_ids.include?(from)
-                     URI.escape("#{item.name} [#{accounts[from]}] #{number_to_currency(item.amount)}")
-                   elsif item.user.expense_ids.include?(to)
-                     URI.escape("#{item.name} [#{accounts[to]}] #{number_to_currency(item.amount)}")
-                   else
-                     URI.escape("#{item.name} #{number_to_currency(item.amount)}")
-                   end
+    text = if item.user.income_ids.include?(from)
+             "#{item.name} [#{accounts[from]}] #{number_to_currency(item.amount)}"
+           elsif item.user.expense_ids.include?(to)
+             "#{item.name} [#{accounts[to]}] #{number_to_currency(item.amount)}"
+           else
+             "#{item.name} #{number_to_currency(item.amount)}"
+           end
 
-    tags = item.tags.to_a.sort { |a, b| a.name <=> b.name }
+    tags = item.tags.to_a.sort_by(&:name)
     tags << 'sanataro'
-    escaped_hashtags = URI.escape(tags.join(','))
+    hashtags = tags.join(',')
 
-    link_to(url_for("#{TWITTER_SHARE_URL}?url=#{escaped_url}&text=#{escaped_text}&hashtags=#{escaped_hashtags}&source=tweetbutton&lang=ja"), class: 'tweet_button', onclick: "open_twitter(this.getAttribute('href'));return false;") { image_tag('twitter_icon.png', alt: 'Tweet', class: 'tweet_icon') }
+    uri = URI(TWITTER_SHARE_URL)
+    uri.query = {
+      url: url,
+      text: text,
+      hashtags: hashtags,
+      source: 'tweetbutton',
+      lang: 'ja'
+    }.to_param
+
+    link_to(uri.to_s, class: 'tweet_button', onclick: "open_twitter(this.getAttribute('href'));return false;") do
+      image_tag('twitter_icon.png', alt: 'Tweet', class: 'tweet_icon')
+    end
   end
 end
