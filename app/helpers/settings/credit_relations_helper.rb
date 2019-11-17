25cc24addbd5a01b025fcfb6fc044bffb2506abd
Kazuya NUMATA <numata@gmail.com>
2019-11-17 17:34:58 +0900

    Shut rubocop up

--- app/helpers/settings/credit_relations_helper.rb
+++ app/helpers/settings/credit_relations_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-module Settings::CreditRelationsHelper
-  def localize_relative_month(relative_month)
-    CreditRelation::PAYMENT_MONTHS.find { |pm| pm[1] == relative_month }[0]
+module Settings
+  module CreditRelationsHelper
+    def localize_relative_month(relative_month)
+      CreditRelation::PAYMENT_MONTHS.find { |pm| pm[1] == relative_month }[0]
+    end
   end
 end
