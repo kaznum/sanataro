8362801a254089fd8f904b5aa6ca99c068d0b88c
Kazuya NUMATA <numata@gmail.com>
2019-11-17 17:12:08 +0900

    Shut rubocop up

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 module TwitterHelper
-  TWITTER_SHARE_URL = "http://twitter.com/intent/tweet"
+  TWITTER_SHARE_URL = 'http://twitter.com/intent/tweet'
   def tweet_button(item)
-    escaped_url = URI.escape("http://bit.ly/Imx6PT")
+    escaped_url = URI.escape('http://bit.ly/Imx6PT')
     accounts = item.user.all_accounts
     from = item.from_account_id
     to = item.to_account_id
 
-    if item.user.income_ids.include?(from)
-      escaped_text = URI.escape("#{item.name} [#{accounts[from]}] #{number_to_currency(item.amount)}")
-    elsif item.user.expense_ids.include?(to)
-      escaped_text = URI.escape("#{item.name} [#{accounts[to]}] #{number_to_currency(item.amount)}")
-    else
-      escaped_text = URI.escape("#{item.name} #{number_to_currency(item.amount)}")
-    end
+    escaped_text = if item.user.income_ids.include?(from)
+                     URI.escape("#{item.name} [#{accounts[from]}] #{number_to_currency(item.amount)}")
+                   elsif item.user.expense_ids.include?(to)
+                     URI.escape("#{item.name} [#{accounts[to]}] #{number_to_currency(item.amount)}")
+                   else
+                     URI.escape("#{item.name} #{number_to_currency(item.amount)}")
+                   end
 
     tags = item.tags.to_a.sort { |a, b| a.name <=> b.name }
-    tags << "sanataro"
-    escaped_hashtags = URI.escape(tags.join(","))
+    tags << 'sanataro'
+    escaped_hashtags = URI.escape(tags.join(','))
 
-    link_to(url_for("#{TWITTER_SHARE_URL}?url=#{escaped_url}&text=#{escaped_text}&hashtags=#{escaped_hashtags}&source=tweetbutton&lang=ja"), class: "tweet_button", onclick: "open_twitter(this.getAttribute('href'));return false;") { image_tag("twitter_icon.png", alt: "Tweet", class: "tweet_icon") }
+    link_to(url_for("#{TWITTER_SHARE_URL}?url=#{escaped_url}&text=#{escaped_text}&hashtags=#{escaped_hashtags}&source=tweetbutton&lang=ja"), class: 'tweet_button', onclick: "open_twitter(this.getAttribute('href'));return false;") { image_tag('twitter_icon.png', alt: 'Tweet', class: 'tweet_icon') }
   end
 end
