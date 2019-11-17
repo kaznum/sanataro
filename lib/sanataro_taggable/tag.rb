7fbd9ac3a06a07b02ec6e53ec616f3004ebd8852
Kazuya NUMATA <numata@gmail.com>
2019-11-17 12:31:32 +0900

    Replace destructive usage of tag string

--- lib/sanataro_taggable/tag.rb
+++ lib/sanataro_taggable/tag.rb
@@ -2,15 +2,15 @@ class Tag < ActiveRecord::Base
   has_many :taggings
   validates_presence_of :name
 
-  def self.parse(list)
+  def self.parse(tags_str)
     tag_names = []
-    return tag_names if list.blank?
+    return tag_names if tags_str.blank?
 
-    list.gsub!(/\"(.*?)\"\s*/) do
+    tags_str = tags_str.gsub(%r|\"(.*?)\"\s*|) do
       tag_names << $1
-      ""
+      ''
     end
-    tag_names.concat(list.gsub(/,/, " ").split(/\s/)).delete_if { |t| t.empty? }.map(&:downcase).uniq
+    (tag_names | tags_str.gsub(%r|,|, ' ').split(%r|\s|)).map(&:presence).compact.map(&:downcase).uniq
   end
 
   def to_s
