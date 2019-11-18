77b049e9247137d4d9684a51877c8eb640c25227
Kazuya NUMATA <numata@gmail.com>
2019-11-18 20:52:25 +0900

    Shut rubocop up by rubocop --only Style/LineEndConcatenation -a

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -28,7 +28,7 @@ module NavigationHelpers
         path_components = $1.split(/\s+/)
         send(path_components.push('path').join('_').to_sym)
       rescue NoMethodError, ArgumentError
-        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" \
           "Now, go and add a mapping in #{__FILE__}"
       end
     end
