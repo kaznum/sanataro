a3d91df86fcf1882eca7b21cdda4cece0a6caffa
Kazuya NUMATA <numata@gmail.com>
2019-11-18 21:29:39 +0900

    Shut rubocop up by rubocop --only Style/ColonMethodCall

--- features/support/env.rb
+++ features/support/env.rb
@@ -94,10 +94,10 @@ end
 Capybara.javascript_driver = :webkit
 
 Before do
-  ActiveRecord::FixtureSet::reset_cache
+  ActiveRecord::FixtureSet.reset_cache
   fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
   fixtures = Dir[File.join(fixtures_folder, '*.yml')].map { |f| File.basename(f, '.yml') }
-  ActiveRecord::FixtureSet::create_fixtures(fixtures_folder, fixtures)
+  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
 end
 
 Capybara.default_max_wait_time = ENV['TRAVIS'] ? 60 : 5
