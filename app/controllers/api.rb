9230f2832e25f9c2659bf94e8cd61572e8938599
Kazuya NUMATA <numata@gmail.com>
2019-11-18 21:45:57 +0900

    Use safe navigation &.

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -21,7 +21,7 @@ module Api
           # for Basic Auth login
           user = authenticate_with_http_basic do |login, password|
             challenge_user = User.find_by_login_and_active(login, true)
-            challenge_user && challenge_user.password_correct?(password) ? challenge_user : nil
+            challenge_user&.password_correct?(password) ? challenge_user : nil
           end
         end
 
